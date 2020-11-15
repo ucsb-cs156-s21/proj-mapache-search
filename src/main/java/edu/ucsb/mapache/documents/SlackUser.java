9a0413452414faa9d25c059e232f6c24a2105a4d
Tanay Komarlu <tkomarlu@ucsb.edu>
2020-11-15 12:01:21 -0800

    tk - reorganized SlackUser code

--- src/main/java/edu/ucsb/mapache/documents/SlackUser.java
+++ src/main/java/edu/ucsb/mapache/documents/SlackUser.java
@@ -8,20 +8,38 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "users")
 public class SlackUser {
     private String id;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     private String name;
     private String real_name;
     private SlackUserProfile profile;
 
-    public SlackUser(){
+    public SlackUser(){ }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReal_name() {
+        return real_name;
+    }
+
+    public void setReal_name(String real_name) {
+        this.real_name = real_name;
+    }
+
+    public SlackUserProfile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(SlackUserProfile profile) {
+        this.profile = profile;
     }
 
     @Override
@@ -50,27 +68,4 @@ public class SlackUser {
                 '}';
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getReal_name() {
-        return real_name;
-    }
-
-    public void setReal_name(String real_name) {
-        this.real_name = real_name;
-    }
-
-    public SlackUserProfile getProfile() {
-        return profile;
-    }
-
-    public void setProfile(SlackUserProfile profile) {
-        this.profile = profile;
-    }
 }
