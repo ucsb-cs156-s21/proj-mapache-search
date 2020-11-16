80fb48c5019a7809e2cd42efd26d309d095e694d
Scott Chow <tehchowster@gmail.com>
2020-11-15 17:44:14 -0800

    sc - create Authorized Route to prevent conditonal routing bugs

--- javascript/src/main/App.js
+++ javascript/src/main/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "main/App.css";
-import Loading from "main/components/Loading/Loading";
 import AppNavbar from "main/components/Nav/AppNavbar";
-import { useAuth0 } from "@auth0/auth0-react";
 import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
 import AppFooter from "main/components/Footer/AppFooter";
@@ -11,37 +9,20 @@ import Home from "main/pages/Home/Home";
 import Profile from "main/pages/Profile/Profile";
 import PrivateRoute from "main/components/Auth/PrivateRoute";
 import Admin from "main/pages/Admin/Admin";
-import useSWR from "swr";
-import { fetchWithToken } from "main/utils/fetch";
 import SlackUsers from "./pages/Admin/SlackUsers";
+import AuthorizedRoute from "main/components/Nav/AuthorizedRoute";
 
 function App() {
-  const { isLoading, getAccessTokenSilently: getToken } = useAuth0();
-  const { data: roleInfo } = useSWR(
-    ["/api/myRole", getToken],
-    fetchWithToken
-  );
-  const isAdmin = roleInfo && roleInfo.role.toLowerCase() === "admin";
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
     <div className="App">
       <AppNavbar />
       <Container className="flex-grow-1 mt-5">
         <Switch>
-          <Route path="/" exact component={Home} />
+          <AuthorizedRoute path="/admin" exact component={Admin} authorizedRoles={["admin"]} />
+          <AuthorizedRoute path="/admin/slackUsers" exact component={SlackUsers} authorizedRoles={["admin"]} />
           <PrivateRoute path="/profile" component={Profile} />
-          { isAdmin &&
-              <>
-                <PrivateRoute path="/admin" exact component={Admin} />
-                <PrivateRoute path="/admin/slackUsers" exact component={SlackUsers} />
-              </>
-          }
-
           <Route path="/about" component={About} />
+          <Route path="/" exact component={Home} />
         </Switch>
       </Container>
       <AppFooter />
