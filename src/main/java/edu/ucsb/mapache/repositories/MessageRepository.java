ea5ac21aa18a8e6ab6eda79b79e86531c6ab067c
shouc <shou@ucsb.edu>
2020-11-30 19:38:28 -0800

    [shou] add message route

---
+++ src/main/java/edu/ucsb/mapache/repositories/MessageRepository.java
@@ -0,0 +1,14 @@
+package edu.ucsb.mapache.repositories;
+
+import edu.ucsb.mapache.documents.Message;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends MongoRepository<Message, ObjectId> {
+    @Query("{ 'user': ?0}")
+    List<Message> findByUser(String user);
+}
