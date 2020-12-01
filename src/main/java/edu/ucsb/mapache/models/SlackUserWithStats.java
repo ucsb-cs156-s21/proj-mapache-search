5ad56417ec469f0e90f1136048bdafa2c41d56e7
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-11-30 20:08:56 -0800

    pc - WIP from Monday's lecture ... just illustration of how to go about doing message counts

---
+++ src/main/java/edu/ucsb/mapache/models/SlackUserWithStats.java
@@ -0,0 +1,69 @@
+package edu.ucsb.mapache.models;
+
+import edu.ucsb.mapache.documents.SlackUser;
+
+public class SlackUserWithStats {
+    private SlackUser slackUser;
+    private int messageCount;
+
+    public SlackUserWithStats() {
+    }
+
+    public SlackUserWithStats(SlackUser slackUser, int messageCount) {
+        this.slackUser = slackUser;
+        this.messageCount = messageCount;
+    }
+
+    public SlackUser getSlackUser() {
+        return this.slackUser;
+    }
+
+    public void setSlackUser(SlackUser slackUser) {
+        this.slackUser = slackUser;
+    }
+
+    public int getMessageCount() {
+        return this.messageCount;
+    }
+
+    public void setMessageCount(int messageCount) {
+        this.messageCount = messageCount;
+    }
+
+    public SlackUserWithStats slackUser(SlackUser slackUser) {
+        this.slackUser = slackUser;
+        return this;
+    }
+
+    public SlackUserWithStats messageCount(int messageCount) {
+        this.messageCount = messageCount;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof SlackUserWithStats)) {
+            return false;
+        }
+        SlackUserWithStats slackUserWithStats = (SlackUserWithStats) o;
+        return Objects.equals(slackUser, slackUserWithStats.slackUser) && messageCount == slackUserWithStats.messageCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(slackUser, messageCount);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " slackUser='" + getSlackUser() + "'" +
+            ", messageCount='" + getMessageCount() + "'" +
+            "}";
+    }
+
+
+
+}
