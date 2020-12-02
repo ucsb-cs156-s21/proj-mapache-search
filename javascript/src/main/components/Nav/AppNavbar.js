4302274a927fe299406566988410f5acffbc0d70
neenbeannnn <hmn.nini1970@gmail.com>
2020-12-02 00:32:08 -0800

    nh - changed routes to authenticated routes

--- javascript/src/main/components/Nav/AppNavbar.js
+++ javascript/src/main/components/Nav/AppNavbar.js
@@ -23,21 +23,27 @@ function AppNavbar() {
         <Navbar.Brand data-testid="brand">Mapache Search</Navbar.Brand>
       </LinkContainer>
       <Nav>
-          { isAdmin &&
-              <NavDropdown title="Admin">
-                  <NavDropdown.Item href="/admin">Maintain Admins</NavDropdown.Item>
-                  <NavDropdown.Item href="/admin/slackUsers">Slack Users</NavDropdown.Item>
-              </NavDropdown>
-          }
-          { (isMember || isAdmin)  &&
-              <NavDropdown title="Channels">
-                  <NavDropdown.Item href="/member/channels">List Channels</NavDropdown.Item>
-              </NavDropdown>
-          }
+        {isAdmin &&
+          <NavDropdown title="Admin">
+            <NavDropdown.Item href="/admin">Maintain Admins</NavDropdown.Item>
+            <NavDropdown.Item href="/admin/slackUsers">Slack Users</NavDropdown.Item>
+          </NavDropdown>
+        }
+        {(isMember || isAdmin) &&
+          <NavDropdown title="Channels">
+            <NavDropdown.Item href="/member/channels">List Channels</NavDropdown.Item>
+          </NavDropdown>
+        }
         <LinkContainer to={"/about"}>
-            <Nav.Link>About</Nav.Link>
+          <Nav.Link>About</Nav.Link>
         </LinkContainer>
         <ProfileNav />
+        <NavDropdown title="Analyze Message Data">
+          <NavDropdown.Item href="/member/analyzemessages/reactions">Analyze Reactions</NavDropdown.Item>
+          <NavDropdown.Item href="/member/analyzemessages/countmessages">Count Messages By User</NavDropdown.Item>
+          <NavDropdown.Item href="/member/analyzemessages/messagehistogram">Histogram of Messages for a User</NavDropdown.Item>
+          <NavDropdown.Item href="/member/analyzemessages/searchmessages">Search Messages By User</NavDropdown.Item>
+        </NavDropdown>
       </Nav>
       <Navbar.Collapse className="justify-content-end">
         <AuthNav />
