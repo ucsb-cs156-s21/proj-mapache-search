101c8bdec31fe6b54ed78941041fd7b14a5b6cf0
Nicholas Brown <countdooku.steak@gmail.com>
2020-12-02 14:20:35 -0800

    nb - updated the ChannelPage to get the messages from the backend and display them in a MessageList

--- javascript/src/main/pages/Channels/ChannelPage.js
+++ javascript/src/main/pages/Channels/ChannelPage.js
@@ -2,22 +2,20 @@ import React from "react";
 import { Jumbotron } from "react-bootstrap";
 import {useAuth0} from "@auth0/auth0-react";
 import { Redirect } from "react-router-dom";
-import ChannelMessages from "main/components/Channels/ChannelMessages"
 import useSWR from "swr";
 import { useParams} from "react-router-dom";
 import {fetchWithToken} from "main/utils/fetch";
+import MessageList from "main/components/Messages/MessageList"
 
 const ChannelPage = () => {
     const { getAccessTokenSilently: getToken } = useAuth0();
     const { channel } = useParams();
+    const { data: messages } = useSWR([`/api/members/channel/${channel}/messages`, getToken], fetchWithToken);
     return (
-           <>
-           <h1> {channel} </h1>
-           {
-            <p>Messages go here</p>
-
-           }
-         </>
+        <>
+            <h1> {channel} </h1>
+            <MessageList messages={messages || []} />
+        </>
     );
 };
 
