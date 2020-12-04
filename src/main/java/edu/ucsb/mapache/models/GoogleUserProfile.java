8608993eacd99c3282b98bea39d22b42a62efdc7
Christopher Barnett <christopherbarnett@ucsb.edu>
2020-12-04 15:52:22 -0800

    cb - Custom API instance variable added and implemented getter and setter

--- src/main/java/edu/ucsb/mapache/models/GoogleUserProfile.java
+++ src/main/java/edu/ucsb/mapache/models/GoogleUserProfile.java
@@ -25,6 +25,9 @@ public class GoogleUserProfile {
   private String locale;
   @JsonProperty("updated_at")
   private String updatedAt;
+  // CB - Custom API instance variable 
+  @JsonProperty("custom_api")
+  private String customAPI;
 
   /**
    * @param json - the json string of the object being created
@@ -113,6 +116,16 @@ public class GoogleUserProfile {
 
   public void setUpdatedAt(String updatedAt) {
     this.updatedAt = updatedAt;
+  }
+
+  // CB - Custom API getter
+  public String getCustomAPI() {
+    return this.customAPI;
+  }
+
+  // CB - Custom API setter
+  public void setCustomAPI() {
+    this.customAPI = customAPI;
   }
 
   public String toJSONString() throws Exception {
