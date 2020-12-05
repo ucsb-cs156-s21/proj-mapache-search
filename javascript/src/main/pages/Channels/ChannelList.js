81e42eaa960b2d9143eb108ad5ecd8b5db9d494f
DanielEskander <deskander@ucsb.edu>
2020-12-04 19:10:43 -0800

    DE - reworked the search bar so that it sends the user to a search result page only if the user enters input and the input is saved in a variable that will be used in the backend

--- javascript/src/main/pages/Channels/ChannelList.js
+++ javascript/src/main/pages/Channels/ChannelList.js
@@ -6,26 +6,35 @@ import ChannelTable from "main/components/Channels/ChannelTable"
 import useSWR from "swr";
 import {fetchWithToken} from "main/utils/fetch";
 
-
-
 const ChannelList = () => {
     const { getAccessTokenSilently: getToken } = useAuth0();
     const { data: channels } = useSWR(["/api/members/channels", getToken], fetchWithToken);
 
+    function getInputValue(e) {
+        // If the user doesn't enter anything in the search bar the user will stay on the channel list page
+        if(document.getElementById("myInput").value.length == 0){
+            return;
+        }
+        else{
+            // Saves the user's input in inputVal and sends the user to the SearchResults Page
+            var inputVal = document.getElementById("myInput").value;
+            window.location.href = `/member/channels/SearchResults`;
+        }
+    }
+    
     return (
-       // <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-
+                // form allows you to type into the search bar
                 <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
                 <div class="input-group">
-
-                    <input class="form-control" type="text" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
+                    <input class="form-control" type="text" id = "myInput" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
                     <div class="input-group-append">
-                        <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                        {/* Uses boostrap to make the search button which stores user input inputVal in the function getInputValue */}
+                        <button class="btn btn-primary" type="button" onClick={getInputValue}><i class="fas fa-search"></i>Search</button>
                     </div>
+                     {/* Lists out all of the channels with their respective data */}
                     <ChannelTable channels={channels || []} />
                 </div>
-            </form>      
-        
+            </form>
     );
 };
 
