562fd55048ecabe69a7e903d00a431e953397c7c
Nicholas Brown <countdooku.steak@gmail.com>
2020-12-05 16:30:48 -0800

    nb - incorporated tests that were merged into main since last commit

--- javascript/src/test/components/Channels/ChannelTable.test.js
+++ javascript/src/test/components/Channels/ChannelTable.test.js
@@ -3,25 +3,35 @@ import { render } from "@testing-library/react";
 import ChannelTable from "main/components/Channels/ChannelTable";
 
 describe("ChannelTable tests", () => {
-    test("renders without crashing", () => {
-        render(<ChannelTable channels={[]} />);
-    });
+  test("renders without crashing", () => {
 
-    test("includes link to channel", () => {
-        const exampleChannel = {
-            'id': 1,
-            'name' : 'test-name',
-            'purpose': {
-                'value': 'Test Purpose'
+    const channels = [
+        {
+            "id": "C016GMB0H5L",
+            "name": "section-6pm",
+            "creator": "U017218J9B3",
+            "is_archived": false,
+            "is_general": false,
+            "members": [
+                "U017218J9B3",
+                "U0185QQSJBY",
+                "U018CH1NLPL",
+                "U018R1AULF3",
+                "U018XEKMRTM",
+                "U0192BC785N",
+                "U019B1Q0YHW"
+            ],
+            "topic": {
+                "value": "",
+                "creator": "",
             },
-            'topic': {
-                'value': 'Test Value'
+            "purpose": {
+                "value": "",
+                "creator": "",
             }
-        };
-        const expectedLink = `/member/channels/${exampleChannel.name}`;
-        const { getByText} = render(<ChannelTable channels={[exampleChannel]} />);
-        const linkElement = getByText(/click here/);
-        expect(linkElement.href).toMatch(expectedLink);
-    });
+        }
+    ];
 
+    render(<ChannelTable channels={channels}/>);
+  });
 });
--- javascript/src/test/pages/Channels/ChannelList.test.js
+++ javascript/src/test/pages/Channels/ChannelList.test.js
@@ -1,37 +1,68 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import ChannelList from "main/pages/Channels/ChannelList";
+
+
+import { useAuth0 } from "@auth0/auth0-react";
+jest.mock("@auth0/auth0-react");
 import useSWR from "swr";
 jest.mock("swr");
+import { fetchWithToken } from "main/utils/fetch";
+jest.mock("main/utils/fetch");
 
 describe("ChannelList tests", () => {
-    test("renders without crashing", () => {
-        useSWR.mockReturnValue({});
+
+    const channels = [
+        {
+            "id": "C016GMB0H5L",
+            "name": "section-6pm",
+            "creator": "U017218J9B3",
+            "is_archived": false,
+            "is_general": false,
+            "members": [
+                "U017218J9B3",
+                "U0185QQSJBY",
+                "U018CH1NLPL",
+                "U018R1AULF3",
+                "U018XEKMRTM",
+                "U0192BC785N",
+                "U019B1Q0YHW"
+            ],
+            "topic": {
+                "value": "",
+                "creator": "",
+            },
+            "purpose": {
+                "value": "",
+                "creator": "",
+            }
+        }
+    ];
+
+    beforeEach(() => {
+        useAuth0.mockReturnValue({
+          getAccessTokenSilently: jest.fn(),
+        });
+      });
+
+    const mockBackend = (results) => {
+        useSWR.mockImplementation(([endpoint, getToken], fetch) => {
+            if (endpoint === "/api/members/channels")
+              return {
+                data: results,
+              };
+            else
+                fail(`test called on unexpected endpoint: ${endpoint}`);
+          });
+    }
+
+    test("renders empty list without crashing", () => {
+        mockBackend(null);
         render(<ChannelList />);
     });
-    
-    test("Accesses channels from backend", () => {
-        const exampleChannel = {
-            'id': 1,
-            'name' : 'test-name',
-            'purpose': {
-                'value': 'Test Purpose'
-            },
-            'topic': {
-                'value': 'Test Value'
-            }
-        };
 
-        useSWR.mockReturnValue({
-            'data': [exampleChannel]
-        });
-        const { getByText } = render(<ChannelList />);
-        const nameElement = getByText(/test-name/);
-        const purposeElement = getByText(/Test Purpose/);
-        const topicElement = getByText(/Test Value/);
-        expect(nameElement).toBeInTheDocument();
-        expect(purposeElement).toBeInTheDocument();
-        expect(topicElement).toBeInTheDocument();
+    test("renders list with results without crashing", () => {
+        mockBackend(channels);
+        render(<ChannelList />);
     });
-
 });
