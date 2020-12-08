82eafcb3cac46c7f195e4dc7e79cb0df79815e7a
Nicholas Brown <countdooku.steak@gmail.com>
2020-12-07 17:44:46 -0800

    nb/lp - Replaced Slack Ids with real names in the message display

--- javascript/src/main/components/ChannelMessages/MessageList.js
+++ javascript/src/main/components/ChannelMessages/MessageList.js
@@ -1,10 +1,28 @@
 import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
+import useSWR from "swr";
+import {useAuth0} from "@auth0/auth0-react";
+import {fetchWithToken} from "main/utils/fetch";
+
+const userName = (message) => {
+    let userProfile = message.user_profile;
+    let result = "";
+    if(userProfile == null) {
+        result += message.user;
+    } else {
+        result += userProfile.real_name;
+    }
+    return(
+        <p>{result}</p>
+    );
+}
 
 export default ({ messages }) => {
     const columns = [{
-        dataField: 'user',
-        text: 'user'
+        isDummyField: true,
+        formatter: (cell, row) => userName(row),
+        dataField: 'name',
+        text: 'Username'
     },{
         dataField: 'text',
         text: 'Contents'
--- javascript/src/test/components/ChannelMessages/MessageList.test.js
+++ javascript/src/test/components/ChannelMessages/MessageList.test.js
@@ -6,4 +6,21 @@ describe("MessageList tests", () => {
     test("renders without crashing", () => {
         render(<MessageList messages={[]} />);
     });
+
+    test("Displays username", () => {
+        const exampleMessage = {
+            "type": "message",
+            "subtype": "channel_join",
+            "ts": "1594143066.000200",
+            "user": "U017218J9B3",
+            "text": "<@U017218J9B3> has joined the channel",
+            "channel": "section-6pm",
+            "user_profile": {
+                "real_name": "Test Person"
+            }
+        }
+        const {getByText} = render(<MessageList messages={[exampleMessage]}/>);
+        const nameElement = getByText(/Test Person/);
+        expect(nameElement).toBeInTheDocument();
+    });
 });
