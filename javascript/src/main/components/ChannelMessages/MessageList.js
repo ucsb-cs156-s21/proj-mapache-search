00cf50a38402b030c989c60c28dab0e2422b679c
shouc <shou@ucsb.edu>
2020-12-07 17:01:21 -0800

    cs - add sorting and searching

--- javascript/src/main/components/ChannelMessages/MessageList.js
+++ javascript/src/main/components/ChannelMessages/MessageList.js
@@ -1,17 +1,56 @@
 import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+const { SearchBar } = Search;
 
 export default ({ messages }) => {
     const columns = [{
         dataField: 'user',
-        text: 'user'
+        text: 'user',
+        sort: true
     },{
         dataField: 'text',
-        text: 'Contents'
-    }
+        text: 'Contents',
+        sort: true
+    },
+        {
+            dataField: 'ts',
+            text: 'Time',
+            sort: true,
+            formatter: (cell, _) => {
+                // convert timestamp to human readable time
+                let date = new Date(cell * 1000);
+                return date.getUTCFullYear() +
+                    '-' + ('0' + date.getMonth()).slice(-2) +
+                    '-' + ('0' + date.getDate()).slice(-2) +
+                    ' ' + ('0' + date.getHours()).slice(-2) +
+                    ':' + ('0' + date.getMinutes()).slice(-2) +
+                    ':' + ('0' + date.getSeconds()).slice(-2)
+            },
+            style: {
+                width: "20%"
+            }
+        }
     ];
 
     return (
-        <BootstrapTable keyField='ts' data={messages} columns={columns}/>
+        <div style={{textAlign: "left"}}>
+            <ToolkitProvider
+                keyField="ts"
+                data={ messages }
+                columns={ columns }
+                search
+            >
+                {
+                    props => (
+                        <div>
+                            <SearchBar { ...props.searchProps } />
+                            <hr />
+                            <BootstrapTable { ...props.baseProps } />
+                        </div>
+                    )
+                }
+            </ToolkitProvider>
+        </div>
     );
 };
