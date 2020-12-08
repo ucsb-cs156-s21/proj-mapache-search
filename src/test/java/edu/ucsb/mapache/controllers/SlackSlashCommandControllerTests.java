b7b039470582bb81941f42cb40f12c6d30b959b3
Bryan <bryanxu@ucsb.edu>
2020-12-07 20:34:20 -0800

    bx - updated SlackSlashCommandControllerTests.java for better code coverage

--- src/test/java/edu/ucsb/mapache/controllers/SlackSlashCommandControllerTests.java
+++ src/test/java/edu/ucsb/mapache/controllers/SlackSlashCommandControllerTests.java
@@ -180,27 +180,4 @@ public class SlackSlashCommandControllerTests {
         .andExpect(status().is(200));
   }
 
-
-// google search test below
-  @Test
-  public void test_googleSearch() throws Exception {
-        // content type: https://api.slack.com/interactivity/slash-commands
-    mockMvc
-        .perform(post(testURL).contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                
-                .param("token", slackSlashCommandController.getSlackToken())
-
-        .param("team_id", "value")
-        .param("team_domain", "value")
-        .param("channel_id", "value")
-        .param("channel_name", "value")
-        .param("user_id", "value")
-        .param("user_name", "value")
-        .param("command", "/mapache")
-        .param("text", "search google 0")
-        .param("response_url", "value")
-        )
-        .andExpect(status().is(200));
-  }
-
 }
