06c2a9291a38b4d7ef1ef5c236f5f20afb87bfec
shouc <shou@ucsb.edu>
2020-12-09 11:42:06 -0800

    cs - add more test cases to search result

--- javascript/src/test/pages/Messages/SearchResults.test.js
+++ javascript/src/test/pages/Messages/SearchResults.test.js
@@ -4,50 +4,45 @@ import { useParams} from "react-router-dom";
 import SearchResults from "main/pages/Messages/SearchResults";
 import userEvent from "@testing-library/user-event";
 
-// import useSWR from "swr";
-// jest.mock("swr");
-// jest.mock("react-router-dom", () => {
-//     return {
-//         'useParams': jest.fn(),
-//     };
-// });
+import useSWR from "swr";
+jest.mock("swr");
+jest.mock("react-router-dom", () => {
+    return {
+        'useParams': jest.fn(),
+    };
+});
 
 describe("ChannelPageList tests", () => {
-    // beforeEach(() => {
-    //     useParams.mockReturnValue({
-    //         'channel': 'test-channel'
-    //     });
-    // });
 
     test("renders without crashing", () => {
-        //useSWR.mockReturnValue({});
+        useSWR.mockReturnValue({'data': []});
         render(<SearchResults />);
     });
 
     test("renders without crashing", () => {
-        //useSWR.mockReturnValue({});
+        useSWR.mockReturnValue({'data': []});
         const { getByLabelText } = render(<SearchResults />);
         const selectSearchString = getByLabelText("Search String");
         userEvent.type(selectSearchString, "springboot");
     });
 
-//     test("loads messages from the backend", () => {
-//         const exampleMessage = {
-//             "type": "message",
-//             "subtype": "channel_join",
-//             "ts": "1594143066.000200",
-//             "user": "U017218J9B3",
-//             "text": "<@U017218J9B3> has joined the channel",
-//             "channel": "section-6pm"
-//         }
+    test("loads messages from the backend", () => {
+        const exampleMessage = {
+            "type": "message",
+            "subtype": "channel_join",
+            "ts": "1594143066.000200",
+            "user": "U017218J9B3",
+            "text": "<@U017218J9B3> has joined the channel",
+            "channel": "section-6pm"
+        }
 
 
-//         useSWR.mockReturnValue({
-//             'data': [exampleMessage]
-//         });
+        useSWR.mockReturnValue({
+            'data': [exampleMessage]
+        });
 
-//         const { getByText } = render(<ChannelPageList />);
-//         const contentsElement = getByText(exampleMessage.text);
-//         expect(contentsElement).toBeInTheDocument();
-//     });
+        const { getByText } = render(<SearchResults />);
+        const contentsElement = getByText(exampleMessage.text);
+        expect(contentsElement).toBeInTheDocument();
+    });
 });
