917e7c53631b6880b5101bdcbd029b200ed0449f
Nicholas Brown <countdooku.steak@gmail.com>
2020-12-08 17:25:10 -0800

    nb/lp - Added username replacement for inside of the message

--- javascript/src/main/components/ChannelMessages/MessageList.js
+++ javascript/src/main/components/ChannelMessages/MessageList.js
@@ -3,27 +3,61 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import useSWR from "swr";
 import {useAuth0} from "@auth0/auth0-react";
 import {fetchWithToken} from "main/utils/fetch";
+import ReactDOMServer from "react-dom/server";
 
-const userName = (message) => {
-    let userProfile = message.user_profile;
-    let result = "";
-    if(userProfile == null) {
-        result += message.user;
-    } else {
-        result += userProfile.real_name;
+const GetUserName = ({userId}) => {
+    const { getAccessTokenSilently: getToken } = useAuth0();
+    const {data: slackUser} = useSWR([`/api/slackUsers/${userId}`, getToken], fetchWithToken);
+    let result;
+    if(slackUser == null || slackUser.length == 0) {
+        result = <p>{userId}</p>;
+        return result;
     }
-    return(
-        <p>{result}</p>
+    result = <p>{slackUser[0].real_name}</p>;
+    return result;
+}
+
+const UserName = (userId) => {
+    return (
+        <GetUserName userId={userId} />
     );
 }
+
+const MessageContents = (text) => {
+    return (
+        <GetMessageContents text={text} />
+    );
+}
+
+const GetMessageContents = ({text}) => {
+    const { getAccessTokenSilently: getToken } = useAuth0();
+    const {data: slackUsers} = useSWR([`/api/slackUsers`, getToken], fetchWithToken);
+    return (
+        <p>
+        {text.replaceAll(/<@([A-Z0-9]{11})>/g, (_,userId) => {
+        if(slackUsers != null) {
+        for(let i = 0; i < slackUsers.length; i++) {
+            if(slackUsers[i].id == userId) {
+                return slackUsers[i].real_name;
+            }
+        }
+        }
+        return userId;
+        })}
+        </p>
+    );
+}
+
 
 export default ({ messages }) => {
     const columns = [{
         isDummyField: true,
-        formatter: (cell, row) => userName(row),
+        formatter: (cell, row) => UserName(row.user),
         dataField: 'name',
         text: 'Username'
     },{
+        isDummyField: true,
+        formatter: (cell, row) => MessageContents(row.text),
         dataField: 'text',
         text: 'Contents'
     }
