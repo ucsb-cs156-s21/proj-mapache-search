c41bc07f28697481763b72246ee22427838603ec
joiiewang <49244047+joiiewang@users.noreply.github.com>
2020-12-10 00:43:35 -0800

    Update aggregateUserMessageCount.js

--- javascript/src/main/utils/aggregateUserMessageCount.js
+++ javascript/src/main/utils/aggregateUserMessageCount.js
@@ -1,10 +1,10 @@
 const aggregateUserMessageCount = (agg_messages, agg_slackUsers) => {
     const userMessageCounts = [];
 
-    var i;
+    let i;
     for(i = 0; i<agg_slackUsers.length;i++){
-        var count = 0;
-        var j;
+        let count = 0;
+        let j;
         for(j=0; j < agg_messages.length; j++) {
             if (agg_messages[j].user_profile !== null && agg_messages[j].user_profile.real_name === agg_slackUsers[i].real_name){
                 count++;
