cb64cf1429c4a665302d46836eb9d199f81232b7
Nina H <nmhuang@ucsb.edu>
2020-12-10 00:54:29 -0800

    Removed comments

--- javascript/src/test/pages/Messages/SearchResults.test.js
+++ javascript/src/test/pages/Messages/SearchResults.test.js
@@ -6,12 +6,10 @@ import userEvent from "@testing-library/user-event";
 
 describe("ChannelPage tests", () => {
     test("renders without crashing", () => {
-        //useSWR.mockReturnValue({});
         render(<SearchResults />);
     });
 
     test("renders without crashing", () => {
-        //useSWR.mockReturnValue({});
         const { getByLabelText } = render(<SearchResults />);
         const selectSearchString = getByLabelText("Search String");
         userEvent.type(selectSearchString, "springboot");
