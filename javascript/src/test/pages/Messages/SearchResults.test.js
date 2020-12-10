20167be4df9701751572a4bb047f3c130aff0258
Nina H <nmhuang@ucsb.edu>
2020-12-10 00:49:18 -0800

    Removing comments

--- javascript/src/test/pages/Messages/SearchResults.test.js
+++ javascript/src/test/pages/Messages/SearchResults.test.js
@@ -4,14 +4,6 @@ import { useParams} from "react-router-dom";
 import SearchResults from "main/pages/Messages/SearchResults";
 import userEvent from "@testing-library/user-event";
 
-// import useSWR from "swr";
-// jest.mock("swr");
-// jest.mock("react-router-dom", () => {
-//     return {
-//         'useParams': jest.fn(),
-//     };
-// });
-
 describe("ChannelPage tests", () => {
     // beforeEach(() => {
     //     useParams.mockReturnValue({
@@ -30,24 +22,4 @@ describe("ChannelPage tests", () => {
         const selectSearchString = getByLabelText("Search String");
         userEvent.type(selectSearchString, "springboot");
     });
-
-//     test("loads messages from the backend", () => {
-//         const exampleMessage = {
-//             "type": "message",
-//             "subtype": "channel_join",
-//             "ts": "1594143066.000200",
-//             "user": "U017218J9B3",
-//             "text": "<@U017218J9B3> has joined the channel",
-//             "channel": "section-6pm"
-//         }
-
-
-//         useSWR.mockReturnValue({
-//             'data': [exampleMessage]
-//         });
-
-//         const { getByText } = render(<ChannelPage />);
-//         const contentsElement = getByText(exampleMessage.text);
-//         expect(contentsElement).toBeInTheDocument();
-//     });
 });
