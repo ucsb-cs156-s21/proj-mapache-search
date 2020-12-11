a63d9a5c3b80def78873c9ca70081dd9e8c7f4b9
ksbenipal <56052694+ksbenipal@users.noreply.github.com>
2020-12-10 17:59:36 -0800

    Delete CountMessagesByUser.test.js

--- javascript/src/test/pages/AnalyzeMessageData/CountMessagesByUser.test.js
+++
@@ -1,8 +0,0 @@
-import CountMessagesByUser from "main/pages/AnalyzeMessageData/CountMessagesByUser";
-import React from "react";
-import { render } from "@testing-library/react";
-describe("CountMessagesByUser tests", () => {
-    test("renders without crashing", () => {
-      render(<CountMessagesByUser/>);
-    });
-});
