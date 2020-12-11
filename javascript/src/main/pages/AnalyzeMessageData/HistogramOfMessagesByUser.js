3d2025547ad0a2b01ac32253f5489d62a743bdb9
neenbeannnn <hmn.nini1970@gmail.com>
2020-12-10 22:23:48 -0800

    removed unnecessary comments

--- javascript/src/main/pages/AnalyzeMessageData/HistogramOfMessagesByUser.js
+++ javascript/src/main/pages/AnalyzeMessageData/HistogramOfMessagesByUser.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
-import useSWR from "swr";
 import { fetchWithToken } from "../../utils/fetch";
 import { useAuth0 } from "@auth0/auth0-react";
 import BootstrapTable from 'react-bootstrap-table-next';
-import { Bar } from "react-chartjs-2";
-import { MDBContainer } from "mdbreact";
 import { Row, Col, Button, Form } from "react-bootstrap";
 
 const columns = [{
@@ -17,31 +14,6 @@ const columns = [{
 }
 ];
 
-// const barChartOptions = {
-//     responsive: true,
-//     maintainAspectRatio: false,
-//     scales: {
-//         xAxes: [
-//             {
-//                 barPercentage: 1,
-//                 gridLines: {
-//                     display: false
-//                 }
-//             }
-//         ],
-//         yAxes: [
-//             {
-//                 gridLines: {
-//                     display: false
-//                 },
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }
-//         ]
-//     }
-// };
-
 const HistogramOfMessagesByUser = () => {
     const [selectedUser, setSelectedUser] = useState('');
     const [userMessages, setUserMessages] = useState([]);
@@ -52,23 +24,6 @@ const HistogramOfMessagesByUser = () => {
     const { getAccessTokenSilently: getToken } = useAuth0();
     var _ = require('lodash');
     var moment = require('moment');
-
-    // const [dataBar, setDataBar] = useState({
-    //     labels: [], //Start date - End date
-    //     datasets: [
-    //         {
-    //             label: "",
-    //             data: [], //Message counts
-    //             backgroundColors: [
-    //                 "rgba(255, 134,159,0.4)"
-    //             ],
-    //             borderWidth: 2,
-    //             borderColor: [
-    //                 "rgba(255, 134, 159, 1)"
-    //             ]
-    //         }
-    //     ]
-    // });
 
     const handleSelectUserSubmit = () => {
         setDataIsFetched(false);
@@ -91,30 +46,6 @@ const HistogramOfMessagesByUser = () => {
                     }
                 ]);
             })
-            //Setting up the bar chart with past 10 weeks
-            // let currentWeek = moment().isoWeek();
-            // console.log(currentWeek)
-            // let firstWeek = currentWeek - 10;
-            // let weekDates = [];
-            // let weekCounts = [];
-            // for (let i = firstWeek; i <= currentWeek; i++) {
-            //     let tmp = perWeekHistogramData.filter((element) => {
-            //         console.log(element.week);
-            //         return (element.week === i);
-            //     });
-            //     if (!tmp) //If no messages sent in that week
-            //         weekCounts.push(0);
-            //     else
-            //         weekCounts.push(tmp.count);
-            //     weekDates.push(tmp.startDate + " - " + tmp.endDate);
-            // }
-            // setDataBar(dataBar => ({
-            //     labels: weekDates,
-            //     datasets: [
-            //         ...dataBar.datasets,
-            //         dataBar.data: weekCounts
-            //     ]
-            // }));
             setDataIsFetched(true);
         }).then(() => {
             modifiedHistogramData.map((element) => {
@@ -156,15 +87,9 @@ const HistogramOfMessagesByUser = () => {
                     <Button id="submit-button" onClick={handleSelectUserSubmit}>Go</Button>
                 </Col>
             </Row>
-            <Button onClick={() => console.log(modifiedHistogramData)}>modifiedHistogramData</Button>
-            <Button onClick={() => console.log(perWeekHistogramData)}>perWeekHistogramData</Button>
-            {/* <Button onClick={() => console.log(dataBar)}>dataBar</Button> */}
             {displayHistogram &&
                 (dataIsFetched ? <div>
                     <h3>Activity for {selectedUser}</h3>
-                    {/* <MDBContainer> */}
-                    {/* <Bar data={dataBar} options={barChartOptions} /> */}
-                    {/* </MDBContainer> */}
                     <BootstrapTable keyField='week' data={perWeekHistogramData || []} columns={columns}></BootstrapTable>
                 </div> : <div className="spinner-border text-primary" role="status">
                         <span className="sr-only">Loading...</span>
