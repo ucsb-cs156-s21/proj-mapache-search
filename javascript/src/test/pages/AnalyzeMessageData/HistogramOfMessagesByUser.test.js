55801fa406a7c3053db8b49ff0c6fcea37fd421a
ksbenipal <56052694+ksbenipal@users.noreply.github.com>
2020-12-10 17:59:28 -0800

    Delete HistogramOfMessagesByUser.test.js

--- javascript/src/test/pages/AnalyzeMessageData/HistogramOfMessagesByUser.test.js
+++
@@ -1,8 +0,0 @@
-import HistogramOfMessagesByUser from "main/pages/AnalyzeMessageData/HistogramOfMessagesByUser";
-import React from "react";
-import { render } from "@testing-library/react";
-describe("HistogramOfMessagesByUser tests", () => {
-    test("renders without crashing", () => {
-      render(<HistogramOfMessagesByUser/>);
-    });
-});
