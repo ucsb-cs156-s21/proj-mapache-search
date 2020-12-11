67b1ed0c53e2382fbc3c3d1bcb1d2957b503b599
ksbenipal <56052694+ksbenipal@users.noreply.github.com>
2020-12-10 17:11:52 -0800

    Update HistogramOfMessagesByUser.test.js

--- javascript/src/test/pages/AnalyzeMessageData/HistogramOfMessagesByUser.test.js
+++ javascript/src/test/pages/AnalyzeMessageData/HistogramOfMessagesByUser.test.js
@@ -1,4 +1,5 @@
 import HistogramOfMessagesByUser from "../../../main/pages/AnalyzeMessageData/HistogramOfMessagesByUser";
+import { waitFor, render } from "@testing-library/react";
 
 describe("HistogramOfMessagesByUser tests", () => {
     test("renders without crashing", () => {
