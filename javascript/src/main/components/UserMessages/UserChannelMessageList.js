da318f3282c610c0edccdf3bd0c3596017165e33
ksbenipal <56052694+ksbenipal@users.noreply.github.com>
2020-12-10 20:33:15 -0800

    Create UserChannelMessageList.js

---
+++ javascript/src/main/components/UserMessages/UserChannelMessageList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+
+export default ({ messages = [] }) => {
+
+    const messageChannels = [];
+
+    const columns = [{
+        dataField: 'channel',
+        text: 'Channel'
+    },{
+        dataField: 'count',
+        text: 'Message Count'
+    }
+    ];
+
+    if(!(messages === undefined) || !(messages.length == 0)) {
+        for (let i = 0; i < messages.length; i++) {
+            if(!messageChannels.some(e => e.channel == messages[i].channel)) {
+                const channelCountPair = {
+                    channel: messages[i].channel,
+                    count: 1
+                }
+                messageChannels.push(channelCountPair);
+            } else {
+                const index = messageChannels.findIndex(channelCountPair => channelCountPair.channel == messages[i].channel);
+                messageChannels[index].count++;
+            }
+        }
+    }
+
+    return (
+        <BootstrapTable keyField='channel' data={messageChannels} columns={columns}/>
+    );
+};
