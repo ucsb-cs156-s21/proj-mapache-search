81fd72d1d8662b7912a13513ccf5bba925ce5735
ksbenipal <56052694+ksbenipal@users.noreply.github.com>
2020-12-10 17:48:49 -0800

    Update CountMessagesByUser.test.js

--- javascript/src/test/pages/AnalyzeMessageData/CountMessagesByUser.test.js
+++ javascript/src/test/pages/AnalyzeMessageData/CountMessagesByUser.test.js
@@ -1,4 +1,5 @@
 import CountMessagesByUser from "main/pages/AnalyzeMessageData/CountMessagesByUser";
+import React from "react";
 import { render } from "@testing-library/react";
 describe("CountMessagesByUser tests", () => {
     test("renders without crashing", () => {
