60e83694c4138c4b7f0ebb59c3c5997b3f311817
ksbenipal <56052694+ksbenipal@users.noreply.github.com>
2020-12-10 17:27:14 -0800

    Update MessageTableReaction.js

--- javascript/src/main/components/ChannelMessageReaction/MessageTableReaction.js
+++ javascript/src/main/components/ChannelMessageReaction/MessageTableReaction.js
@@ -4,14 +4,11 @@ import BootstrapTable  from 'react-bootstrap-table-next';
 export default ({ messages=[],reaction }) => {
 
     function subjectFormatter(cell, row, rowIndex) {
-
         const correctReaction = row.message_reactions.find((message_reaction) => {
             return message_reaction.name === reaction;
         });
         return correctReaction.count;
-    
     }
-
     const columns = [{
         dataField: 'user',
         text: 'User'
@@ -26,9 +23,6 @@ export default ({ messages=[],reaction }) => {
     ];
 
     return (
-     
         <BootstrapTable keyField='ts' data={messages} columns={columns}/>
-
-  
     )
 };
