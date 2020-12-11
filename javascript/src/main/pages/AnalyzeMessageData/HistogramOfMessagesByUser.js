ff02b51b8d0544c553102dad133fca9f23f9d144
ksbenipal <56052694+ksbenipal@users.noreply.github.com>
2020-12-10 17:58:47 -0800

    Delete HistogramOfMessagesByUser.js

--- javascript/src/main/pages/AnalyzeMessageData/HistogramOfMessagesByUser.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { waitFor, render } from "@testing-library/react";
-const HistogramOfMessagesByUser = () => {
-    return <h1>Histogram Of Messages By User</h1>
-};
-
-export default HistogramOfMessagesByUser;
