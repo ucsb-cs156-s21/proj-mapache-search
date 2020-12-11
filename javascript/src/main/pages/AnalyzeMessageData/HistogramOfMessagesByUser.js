d2eca79fd2481cf0d389bcce78eeca894c1077b8
neenbeannnn <hmn.nini1970@gmail.com>
2020-12-10 21:42:44 -0800

    nh - added frontend testing

--- javascript/src/main/pages/AnalyzeMessageData/HistogramOfMessagesByUser.js
+++ javascript/src/main/pages/AnalyzeMessageData/HistogramOfMessagesByUser.js
@@ -1,107 +1,76 @@
 import React, { useState, useEffect } from "react";
-import Select from "react-select";
 import useSWR from "swr";
 import { fetchWithToken } from "../../utils/fetch";
 import { useAuth0 } from "@auth0/auth0-react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import { Bar } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
-import { Row, Col, Button } from "react-bootstrap";
-import { menuListCSS } from "react-select/src/components/Menu";
+import { Row, Col, Button, Form } from "react-bootstrap";
 
 const columns = [{
     dataField: 'week',
     text: 'Week'
 },
 {
-    dataField: 'startDate',
-    text: 'Start Date'
-},
-{
-    dataField: 'endDate',
-    text: 'End Date'
-},
-{
     dataField: 'count',
-    text: 'Count'
+    text: 'Message Count'
 }
 ];
 
-const barChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-        xAxes: [
-            {
-                barPercentage: 1,
-                gridLines: {
-                    display: false
-                }
-            }
-        ],
-        yAxes: [
-            {
-                gridLines: {
-                    display: false
-                },
-                ticks: {
-                    beginAtZero: true
-                }
-            }
-        ]
-    }
-};
-
-
+// const barChartOptions = {
+//     responsive: true,
+//     maintainAspectRatio: false,
+//     scales: {
+//         xAxes: [
+//             {
+//                 barPercentage: 1,
+//                 gridLines: {
+//                     display: false
+//                 }
+//             }
+//         ],
+//         yAxes: [
+//             {
+//                 gridLines: {
+//                     display: false
+//                 },
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }
+//         ]
+//     }
+// };
 
 const HistogramOfMessagesByUser = () => {
     const [selectedUser, setSelectedUser] = useState('');
-    const [sameUser, setSameUser] = useState(false); //Case for when the same user is selected as the currently selected one
     const [userMessages, setUserMessages] = useState([]);
     const [displayHistogram, setDisplayHistogram] = useState(false);
     const [dataIsFetched, setDataIsFetched] = useState(true);
     const [modifiedHistogramData, setModifiedHistogramData] = useState([]);
     const [perWeekHistogramData, setPerWeekHistogramData] = useState([]);
     const { getAccessTokenSilently: getToken } = useAuth0();
-    const { data: slackUsers } = useSWR(["/api/slackUsers", getToken], fetchWithToken);
-    const [userOptions, setUserOptions] = useState([]);
     var _ = require('lodash');
     var moment = require('moment');
 
-    const [dataBar, setDataBar] = useState({
-        labels: [], //Start date - End date
-        datasets: [
-            {
-                label: "",
-                data: [], //Message counts
-                backgroundColors: [
-                    "rgba(255, 134,159,0.4)"
-                ],
-                borderWidth: 2,
-                borderColor: [
-                    "rgba(255, 134, 159, 1)"
-                ]
-            }
-        ]
-    });
-
-    useEffect(() => {
-        if (!slackUsers) setUserOptions([])
-        else {
-            slackUsers.map(i => {
-                setUserOptions(userOptions => [
-                    ...userOptions,
-                    {
-                        value: i.id,
-                        label: i.real_name
-                    }
-                ])
-            })
-        }
-    }, [slackUsers])
+    // const [dataBar, setDataBar] = useState({
+    //     labels: [], //Start date - End date
+    //     datasets: [
+    //         {
+    //             label: "",
+    //             data: [], //Message counts
+    //             backgroundColors: [
+    //                 "rgba(255, 134,159,0.4)"
+    //             ],
+    //             borderWidth: 2,
+    //             borderColor: [
+    //                 "rgba(255, 134, 159, 1)"
+    //             ]
+    //         }
+    //     ]
+    // });
 
     const handleSelectUserSubmit = () => {
-        if (sameUser) return; //Removes the warning for duplicate keys in the BootstrapTable
         setDataIsFetched(false);
         setDisplayHistogram(true);
         setModifiedHistogramData([]);
@@ -122,7 +91,32 @@ const HistogramOfMessagesByUser = () => {
                     }
                 ]);
             })
-            console.log(modifiedHistogramData);
+            //Setting up the bar chart with past 10 weeks
+            // let currentWeek = moment().isoWeek();
+            // console.log(currentWeek)
+            // let firstWeek = currentWeek - 10;
+            // let weekDates = [];
+            // let weekCounts = [];
+            // for (let i = firstWeek; i <= currentWeek; i++) {
+            //     let tmp = perWeekHistogramData.filter((element) => {
+            //         console.log(element.week);
+            //         return (element.week === i);
+            //     });
+            //     if (!tmp) //If no messages sent in that week
+            //         weekCounts.push(0);
+            //     else
+            //         weekCounts.push(tmp.count);
+            //     weekDates.push(tmp.startDate + " - " + tmp.endDate);
+            // }
+            // setDataBar(dataBar => ({
+            //     labels: weekDates,
+            //     datasets: [
+            //         ...dataBar.datasets,
+            //         dataBar.data: weekCounts
+            //     ]
+            // }));
+            setDataIsFetched(true);
+        }).then(() => {
             modifiedHistogramData.map((element) => {
                 let tmp = modifiedHistogramData.filter((elem) => (elem.week === element.week) && (elem.year === element.year));
                 let weekStartDate = moment().isoWeekYear(element.year).isoWeek(element.week).startOf('week').format('MM/DD/YYYY');
@@ -130,77 +124,48 @@ const HistogramOfMessagesByUser = () => {
                 setPerWeekHistogramData(perWeekHistogramData => [
                     ...perWeekHistogramData,
                     {
-                        "week": element.week,
-                        "startDate": weekStartDate,
-                        "endDate": weekEndDate,
+                        "week": weekStartDate + " - " + weekEndDate,
+                        "weekNum": element.week,
                         "count": tmp.length
                     }
                 ])
+                setPerWeekHistogramData(perWeekHistogramData => perWeekHistogramData.sort((a, b) => {
+                    if (a.week < b.week)
+                        return -1;
+                    return 1;
+                }))
                 setPerWeekHistogramData(perWeekHistogramData => _.uniqWith(perWeekHistogramData, _.isEqual));
                 setModifiedHistogramData(modifiedHistogramData => modifiedHistogramData.filter((week) => week !== element));
             })
-            //Setting up the bar chart with past 10 weeks
-            let currentWeek = moment().isoWeek();
-            console.log(currentWeek)
-            let firstWeek = currentWeek - 10;
-            let weekDates = [];
-            let weekCounts = [];
-            for (let i = firstWeek; i <= currentWeek; i++) {
-                let tmp = perWeekHistogramData.filter((element) => {
-                    console.log(element.week);
-                    return (element.week === i);
-                });
-                if (!tmp) //If no messages sent in that week
-                    weekCounts.push(0);
-                else
-                    weekCounts.push(tmp.count);
-                weekDates.push(tmp.startDate + " - " + tmp.endDate);
-            }
-            setDataBar(dataBar => ({
-                labels: weekDates,
-                datasets: [
-                    ...dataBar.datasets,
-                    dataBar.data: weekCounts
-                ]
-            }));
-            setDataIsFetched(true);
-        });
+        })
     }
 
     return (
         <div>
             <h3>Histogram of Messages By User</h3>
-            <p style={{ textAlign: "left" }}>Select a user</p>
             <Row>
                 <Col>
-                    <Select
-                        isSearchable
-                        options={userOptions}
-                        value={userOptions.filter(option => option.label === selectedUser)}
-                        onChange={event => {
-                            if (event.label === selectedUser)
-                                setSameUser(true);
-                            else {
-                                setSameUser(false);
-                                setSelectedUser(event.label)
-                            }
-                        }}
-                    />
+                    <Form.Group controlId="form-select" className="pt-3" style={{ textAlign: "left" }}>
+                        <Form.Label>Enter a user</Form.Label>
+                        <Form.Control
+                            onChange={val => setSelectedUser(val.target.value)}
+                        />
+                    </Form.Group>
                 </Col>
-                <Col xs={1}>
-                    <Button outline id="submit-button" onClick={handleSelectUserSubmit}>Go</Button>
+                <Col xs={2} className="d-flex align-items-center" style={{ marginTop: "35px" }}>
+                    <Button id="submit-button" onClick={handleSelectUserSubmit}>Go</Button>
                 </Col>
             </Row>
             <Button onClick={() => console.log(modifiedHistogramData)}>modifiedHistogramData</Button>
             <Button onClick={() => console.log(perWeekHistogramData)}>perWeekHistogramData</Button>
-            <Button onClick={() => console.log(dataBar)}>dataBar</Button>
+            {/* <Button onClick={() => console.log(dataBar)}>dataBar</Button> */}
             {displayHistogram &&
                 (dataIsFetched ? <div>
                     <h3>Activity for {selectedUser}</h3>
                     {/* <MDBContainer> */}
                     {/* <Bar data={dataBar} options={barChartOptions} /> */}
                     {/* </MDBContainer> */}
-                    {/* <BootstrapTable keyField='week' data={perWeekHistogramData || []} columns={columns}></BootstrapTable> */}
+                    <BootstrapTable keyField='week' data={perWeekHistogramData || []} columns={columns}></BootstrapTable>
                 </div> : <div className="spinner-border text-primary" role="status">
                         <span className="sr-only">Loading...</span>
                     </div>)
--- javascript/src/test/pages/AnalyzeMessageData/HistogramOfMessagesByUser.test.js
+++ javascript/src/test/pages/AnalyzeMessageData/HistogramOfMessagesByUser.test.js
@@ -1,40 +1,49 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import HistogramOfMessagesByUser from "../../../main/pages/AnalyzeMessageData/HistogramOfMessagesByUser";
-
-import useSWR from "swr";
-jest.mock("swr");
-
+import { fireEvent, getAllByTestId, render, getByText, toBeInTheDocument, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import HistogramOfMessagesByUser from "main/pages/AnalyzeMessageData/HistogramOfMessagesByUser";
+import { fetchWithToken } from "main/utils/fetch";
 import { useAuth0 } from "@auth0/auth0-react";
+import useSWR from "swr";
+
 jest.mock("@auth0/auth0-react");
 
-import { fetchWithToken } from "main/utils/fetch";
-jest.mock("main/utils/fetch");
+jest.mock("main/utils/fetch", () => ({
+  fetchWithToken: jest.fn()
+}));
 
 describe("HistogramOfMessagesByUser tests", () => {
   beforeEach(() => {
     useAuth0.mockReturnValue({
-      getAccessTokenSilently: jest.fn(),
+      getAccessTokenSilently: "fakeToken"
     });
   });
-
-  const mockBackend = (results) => {
-    useSWR.mockImplementation(([endpoint, getToken], fetch) => {
-      if (endpoint === `/api/members/messages/usersearch?searchUser=""`)
-        return {
-          data: results,
-        };
-      else
-        fail(`test called on unexpected endpoint: ${endpoint}`);
-    });
-  }
 
   test("renders without crashing", () => {
     render(<HistogramOfMessagesByUser />);
   });
 
-  test("selectedUser state changes when user selects a user and presses go", () => {
-
+  test("renders without crashing on search for user", async () => {
+    const { getByLabelText, getAllByTestId } = render(<HistogramOfMessagesByUser />);
+    const selectSearchUser = getByLabelText("Enter a user");
+    userEvent.type(selectSearchUser, "Test User");
+    expect(selectSearchUser.value).toBe("Test User");
   })
 
+  test("fetch with token is correctly called when user clicks button", async () => {
+    const expectedURL = `/api/members/messages/usersearch?searchUser=Test User`;
+    const options = {
+      method: 'GET'
+    }
+    fetchWithToken.mockResolvedValue([]);
+    const { getByText, getByLabelText } = render(<HistogramOfMessagesByUser />);
+    const selectSearchUser = getByLabelText("Enter a user");
+    userEvent.type(selectSearchUser, "Test User");
+    const goButton = getByText("Go");
+    userEvent.click(goButton);
+    await waitFor(() => {
+      expect(fetchWithToken).toHaveBeenCalledTimes(1);
+      expect(fetchWithToken).toHaveBeenCalledWith(expectedURL, "fakeToken", options);
+    })
+  })
 });
