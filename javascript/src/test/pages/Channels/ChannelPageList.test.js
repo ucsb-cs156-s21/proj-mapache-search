030bd79e20953d2a7b97ee6f715d24dcfc48980a
neenbeannnn <hmn.nini1970@gmail.com>
2020-12-12 14:30:59 -0800

    nh - reinstated ChannelPage.test.js as ChannelPageList.test.js

--- javascript/src/test/pages/Channels/ChannelPageList.test.js
+++ javascript/src/test/pages/Channels/ChannelPageList.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import ChannelPage from "main/pages/Channels/ChannelPage";
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useSWR from "swr";
+
 jest.mock("swr");
 jest.mock("react-router-dom", () => {
     return {
@@ -10,7 +11,7 @@ jest.mock("react-router-dom", () => {
     };
 });
 
-describe("ChannelPage tests", () => {
+describe("ChannelPageList tests", () => {
     beforeEach(() => {
         useParams.mockReturnValue({
             'channel': 'test-channel'
@@ -19,8 +20,14 @@ describe("ChannelPage tests", () => {
 
     test("renders without crashing", () => {
         useSWR.mockReturnValue({});
-        render(<ChannelPage />);
+        render(<ChannelPageList />);
     });
+
+    test("renders without crashing", () => {
+        useSWR.mockReturnValue({});
+        render(<ChannelPageScrollable />);
+    });
+
 
     test("loads messages from the backend", () => {
         const exampleMessage = {
@@ -28,7 +35,7 @@ describe("ChannelPage tests", () => {
             "subtype": "channel_join",
             "ts": "1594143066.000200",
             "user": "U017218J9B3",
-            "text": "<@U017218J9B3> has joined the channel",
+            "text": "Someone has joined the channel",
             "channel": "section-6pm"
         }
 
@@ -37,8 +44,28 @@ describe("ChannelPage tests", () => {
             'data': [exampleMessage]
         });
 
-        const { getByText } = render(<ChannelPage />);
-        const contentsElement = getByText("U017218J9B3 has joined the channel");
+        const { getByText } = render(<ChannelPageList />);
+        const contentsElement = getByText(exampleMessage.text);
+        expect(contentsElement).toBeInTheDocument();
+    });
+
+    test("loads messages from the backend", () => {
+        const exampleMessage = {
+            "type": "message",
+            "subtype": "channel_join",
+            "ts": "1594143066.000200",
+            "user": "U017218J9B3",
+            "text": "Someone has joined the channel",
+            "channel": "section-6pm"
+        }
+
+
+        useSWR.mockReturnValue({
+            'data': [exampleMessage]
+        });
+
+        const { getByText } = render(<ChannelPageScrollable />);
+        const contentsElement = getByText(exampleMessage.text);
         expect(contentsElement).toBeInTheDocument();
     });
 });
