04b8e3ad651f45ac1a1357c6bb44414a32f0a71d
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-02-16 20:40:45 -0800

    Add an About page that recreate the original mapache search About page

--- javascript/src/test/pages/About/About.test.js
+++ javascript/src/test/pages/About/About.test.js
@@ -4,6 +4,11 @@ import About from "main/pages/About/About";
 
 describe("About tests", () => {
   test("renders without crashing", () => {
-    render(<About />);
+    const { getByText } =  render(<About />);
+    expect(getByText("Mapache Search"))
+    expect(getByText("Students can use the app to search for and share helpful resources with their class."))
+    expect(getByText("Instructors can monitor the types of searchers that students are performing, and point them in the right direction."))
+    expect(getByText("Researchers can use the data from Mapache Search to study metacognition - in particular, the way students formulate questions."))
+    expect(getByText("Read More About The Research Project"))
   });
 });
