55e659b7467d6262aef655ac470eb5591a7626e5
Vivian Ross <vivianross06@users.noreply.github.com>
2021-02-16 18:57:34 -0800

    vr - modified AppNavbar.js, removed unnecessary import ProfileNav line

--- javascript/src/main/components/Nav/AppNavbar.js
+++ javascript/src/main/components/Nav/AppNavbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import AuthNav from "main/components/Nav/AuthNav";
-import ProfileNav from "main/components/Nav/ProfileNav";
 import useSWR from "swr";
 import { useAuth0 } from "@auth0/auth0-react";
 import { fetchWithToken } from "main/utils/fetch";
