6dd2a2c30c4759d35e6ddee7cccda25d25b4e362
Kevin Weng <kweng@umail.ucsb.edu>
2021-02-17 19:30:40 -0600

    kw - moved around a space in app footer and added a test for AppFooter

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -4,11 +4,11 @@ const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
       This app is a class project of {" "}
-      <a href="https://ucsb-cs156.github.io/">
-        CMPSC 156 {" "}
+      <a href="https://ucsb-cs156.github.io">
+        CMPSC 156 
         </a> 
-      at {" "}
-      <a href="https://ucsb.edu/">
+      {" "} at {" "}
+      <a href="https://ucsb.edu">
         UCSB
       </a>
       . Check out the source code on {" "}
