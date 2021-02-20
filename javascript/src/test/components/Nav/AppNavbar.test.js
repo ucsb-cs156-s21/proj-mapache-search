d3d1cd502e8a76805583a2a044056696337d3e82
Vivian Ross <vivianross06@users.noreply.github.com>
2021-02-19 20:43:35 -0800

    vr - modified AppNavbar.test.js to remove test looking for profile tab

--- javascript/src/test/components/Nav/AppNavbar.test.js
+++ javascript/src/test/components/Nav/AppNavbar.test.js
@@ -31,15 +31,6 @@ describe("AppNavbar tests", () => {
     const brandElement = getByText(/Mapache Search/);
     expect(brandElement).toBeInTheDocument();
   });
-  test("should have the correct links in the navbar", () => {
-    const history = createMemoryHistory();
-    const { getByText } = render(
-      <Router history={history}>
-        <AppNavbar />
-      </Router>
-    );
-
-  });
 
   test("should render admin links when admin", () => {
     useSWR.mockReturnValue({
