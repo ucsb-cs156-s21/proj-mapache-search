717f1351c4b224c1cc0e219433038c1d45855a8d
Vivian Ross <vivianross06@users.noreply.github.com>
2021-02-27 12:14:32 -0800

    vr - modified MessageScrollableView.js to remove brackets around bracketed elements that are not links and only make http and mailto links clickable

--- javascript/src/main/components/ChannelMessages/MessageScrollableView.js
+++ javascript/src/main/components/ChannelMessages/MessageScrollableView.js
@@ -17,14 +17,29 @@ const replaceMessage = (text, slackUsers) => {
     })
 }
 
-const replaceLink = (text) => {
-    var link = /<(.*?)>/g;
-    return text.replace(link, '<a href = "$1">$1</a>')
+const formatBracketedText = (text) => {
+    var bracketRegEx = /<(.*?)>/g;
+    var found = text.match(bracketRegEx)
+    if (found){
+        for (let i=0; i<found.length; i++){
+            var current = found[i].replace('<', '');
+            current = current.replace('>', '');
+            if (found[i].includes("|") && (found[i].includes("mailto") || found[i].includes("http"))){
+                var links = current.split('|');
+                text = text.replace(found[i], '<a href = ' + links[0] + ' target = "_blank">' + links[1] +'</a>')
+            }else if (found[i].includes("http") || found[i].includes("mailto")){
+                text = text.replace(found[i], '<a href = ' + current + ' target = "_blank">' + current + '</a>')
+            }else{
+                text = text.replace(found[i], current)
+            }
+        }
+    }
+    return text;
 }
 
 const createMarkup = (text, slackUsers) => {
     text = replaceMessage(text, slackUsers)
-    text = replaceLink(text)
+    text = formatBracketedText(text)
     return {
         __html: text
     }
