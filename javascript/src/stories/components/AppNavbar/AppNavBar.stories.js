c5d5aa4fdd9febd3340d08f62ff651738df270bc
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-03-03 17:51:36 -0800

    Finish work on storybook by factoring out visual elements into pure component
    Enable three different views for Navbar storybook

--- javascript/src/stories/components/AppNavbar/AppNavBar.stories.js
+++ javascript/src/stories/components/AppNavbar/AppNavBar.stories.js
@@ -1,36 +1,56 @@
 import React from 'react';
 import { MemoryRouter } from "react-router-dom";
 
-import AppNavbar from "main/components/Nav/AppNavbar";
+import { PureNavbar } from "main/components/Nav/AppNavbar";
 
 export default {
   title: 'components/Nav/AppNavbar',
-  component: AppNavbar
+  component: PureNavbar
 };
 
 const Template = (args) => (
   <MemoryRouter>
-  <AppNavbar {...args}/>
-</MemoryRouter>
+    <PureNavbar {...args} />
+  </MemoryRouter>
 );
+const commonArgs = {
+  ChannelPages: [
+    { link: "/member/channels", name: "List Channels" },
+  ],
+
+  AdminPages: [
+    { link: "/admin", name: "Maintain Admins", },
+    { link: "/admin/slackUsers", name: "Slack Users", },
+    { link: "/admin/teams", name: "Manage Teams", },
+    { link: "/admin/students", name: "Manage Students", },
+    { link: "/admin/searchInfo", name: "Search Information", },
+  ],
+
+  SearchPages: [
+    { link: "/member/messages/search", name: "Slack Search" },
+  ],
+
+  DataPages: [
+    { link: "/member/analyzemessages/reactions", name: "Analyze Reactions", },
+    { link: "/member/analyzemessages/countmessages", name: "Count Messages By User", },
+    { link: "/member/analyzemessages/messagehistogram", name: "Histogram of Messages for a User", },
+    { link: "/member/analyzemessages/searchmessages", name: "Search Messages By User", },
+  ],
+}
 
 export const Guest = Template.bind({});
 Guest.args = {
-  appNavbar: {
-    isMember: false,
-  },
+  ...commonArgs
 };
 
 export const Admin = Template.bind({});
 Admin.args = {
-  appNavbar: {
-    isAdmin: true,
-  },
+  isAdmin: true,
+  ...commonArgs
 };
 
 export const Member = Template.bind({});
 Member.args = {
-  appNavbar: {
-    isMember: true,
-  },
+  isMember: true,
+  ...commonArgs
 };
