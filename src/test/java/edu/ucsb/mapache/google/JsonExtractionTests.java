cc05daa5d405dd399f96fc45d0ea46d086a9695d
Zhao Tang <tangzhao1999@hotmail.com>
2021-03-04 15:52:06 -0800

    more comprehensive tests for objects in google

--- src/test/java/edu/ucsb/mapache/google/JsonExtractionTests.java
+++ src/test/java/edu/ucsb/mapache/google/JsonExtractionTests.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
@@ -26,7 +28,18 @@ public class JsonExtractionTests {
         Path jsonPath = Paths.get("src/test/java/edu/ucsb/mapache/google/sample.json");
         String body = Files.readString(jsonPath);
         SearchResult searchResult = SearchResult.fromJSON(body);
-        assertEquals("Springboot -The AJP Connector is configured with secretRequired ...", searchResult.getItems().get(1).getTitle());
+        Item item = searchResult.getItems().get(1);
+        Item expected = new Item("customsearch#result",
+        "Springboot -The AJP Connector is configured with secretRequired ...",
+        "\u003cb\u003eSpringboot\u003c/b\u003e -The AJP Connector is configured with secretRequired ...",
+        "https://stackoverflow.com/questions/60501470/springboot-the-ajp-connector-is-configured-with-secretrequired-true-but-the-s",
+        "stackoverflow.com",
+        "Here is one solution, though probably not the best one, but my focus was not this, \njust to pass through the error, I was enabling AJP on Spring ...",
+        "Here is one solution, though probably not the best one, but my focus was not this, \u003cbr\u003e\njust to pass through the error, I was enabling AJP on \u003cb\u003eSpring\u003c/b\u003e&nbsp;...",
+        "DhhFMiU1HdYJ",
+        "https://stackoverflow.com/.../springboot-the-ajp-connector-is-configured-with -secretrequired-true-but-the-s",
+        "https://stackoverflow.com/.../\u003cb\u003espringboot\u003c/b\u003e-the-ajp-connector-is-configured-with -secretrequired-true-but-the-s");
+        assertEquals(expected.toString(), item.toString());
     }
 
     @Test
@@ -34,15 +47,41 @@ public class JsonExtractionTests {
         Path jsonPath = Paths.get("src/test/java/edu/ucsb/mapache/google/sample.json");
         String body = Files.readString(jsonPath);
         SearchResult searchResult = SearchResult.fromJSON(body);
-        assertEquals("https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
-        searchResult.getUrl().getTemplate());
+        Url expected = new Url("application/json",
+        "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json");
+        assertEquals(expected.toString(), searchResult.getUrl().toString());
     }
 
     @Test
     public void test_queries_extraction() throws Exception {
         Path jsonPath = Paths.get("src/test/java/edu/ucsb/mapache/google/sample.json");
         String body = Files.readString(jsonPath);
-        SearchResult searchResult = SearchResult.fromJSON(body);;
-        assertEquals("001539284272632380888:kn5n6ubsr7x", searchResult.getQueries().getRequest().get(0).getCx());
+        SearchResult searchResult = SearchResult.fromJSON(body);
+        List<RequestItem> r = new ArrayList<RequestItem>();
+        r.add(new RequestItem(
+        "Google Custom Search - springboot",
+        "408000",
+        "springboot",
+        10,
+        1,
+        "utf8",
+        "utf8",
+        "off",
+        "001539284272632380888:kn5n6ubsr7x"
+        ));
+        List<RequestItem> n = new ArrayList<RequestItem>();
+        n.add(new RequestItem(
+            "Google Custom Search - springboot",
+            "408000",
+            "springboot",
+            10,
+            11,
+            "utf8",
+            "utf8",
+            "off",
+            "001539284272632380888:kn5n6ubsr7x"
+        ));
+        Queries expected = new Queries(r, n);
+        assertEquals(expected.toString(), searchResult.getQueries().toString()); 
     }
 }
