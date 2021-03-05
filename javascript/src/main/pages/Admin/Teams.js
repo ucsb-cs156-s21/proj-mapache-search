581c9fe2fc6b9db3032fc5ec4a6c5500f00f3c09
Kaiwen Li <Kaiwen_li@umail.ucsb.edu>
2021-03-04 21:13:36 -0800

    remove unused line in team.js

--- javascript/src/main/pages/Admin/Teams.js
+++ javascript/src/main/pages/Admin/Teams.js
@@ -1,14 +1,12 @@
 import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 // import { Button } from "react-bootstrap";
-import Table from 'react-bootstrap/Table'
 import useSWR from "swr";
 import {useAuth0} from "@auth0/auth0-react";
 import {fetchWithToken} from "../../utils/fetch";
 
 const Teams = () => {
     const { getAccessTokenSilently: getToken } = useAuth0();
-    const { data: slackUsers } = useSWR(["/api/slackUsers", getToken], fetchWithToken);
     const { data: teamName } = useSWR(["/api/member/teams", getToken], fetchWithToken)
     
     const columns = [{
