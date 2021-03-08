acdc3a21125ec47e2fc83a641d1db7795ad4fb79
chengyanwu <cheng-yan@umail.ucsb.edu>
2021-03-07 16:08:20 -0800

    IW JZ fix no times module issue

--- src/test/java/edu/ucsb/mapache/controllers/SearchControllerTests.java
+++ src/test/java/edu/ucsb/mapache/controllers/SearchControllerTests.java
@@ -15,6 +15,7 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
