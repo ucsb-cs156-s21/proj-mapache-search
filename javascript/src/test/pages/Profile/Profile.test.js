03289e0aad50f54d1c3dd2f97e940ee5fb7beede
Jasun Chen <chen.jasun@gmail.com>
2021-03-09 12:02:34 -0800

    update profile tests to add waitFor

--- javascript/src/test/pages/Profile/Profile.test.js
+++ javascript/src/test/pages/Profile/Profile.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import Profile from "main/pages/Profile/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
 import useSWR from "swr";
