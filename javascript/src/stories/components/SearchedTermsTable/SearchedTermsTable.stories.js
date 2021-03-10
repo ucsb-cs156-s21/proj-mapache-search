cfe0c7a06ab7f3ce36cf6717438e41aa6ff360e2
Adarsh Garg <gargadarsh10@gmail.com>
2021-03-10 13:14:37 -0800

    ag - removed unused vars

---
+++ javascript/src/stories/components/SearchedTermsTable/SearchedTermsTable.stories.js
@@ -0,0 +1,30 @@
+import React from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+
+
+//storybook simulation 
+const SearchedTermsTable = (props) => {
+
+    const columns = [{
+        dataField: 'term',
+        text: 'Term',
+        sort:true
+    }, {
+        dataField: 'id',
+        text: 'ID',
+        sort:true
+    }, {
+        dataField: 'count',
+        text: 'Count',
+        sort:true
+    }];
+
+
+    return (
+        <>
+             <h2>Searched Terms</h2>
+            <BootstrapTable keyField='id' data={props.data} columns={columns} />
+        </>
+    );
+};
+export default SearchedTermsTable;
