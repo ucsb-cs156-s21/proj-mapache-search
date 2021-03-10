367b2a30556897b419cc3f38810f040419908509
Kevin Weng <kweng@umail.ucsb.edu>
2021-03-09 19:56:19 -0800

    kw - footer links open a new tab instead

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -4,15 +4,15 @@ const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
       This app is a class project of {" "}
-      <a href="https://ucsb-cs156.github.io">
+      <a href="https://ucsb-cs156.github.io" target = "_blank">
         CMPSC 156 
         </a> 
       {" "} at {" "}
-      <a href="https://ucsb.edu">
+      <a href="https://ucsb.edu" target = "_blank">
         UCSB
       </a>
       . Check out the source code on {" "}
-      <a href="https://github.com/ucsb-cs156-w21/proj-mapache-search">
+      <a href="https://github.com/ucsb-cs156-w21/proj-mapache-search" target = "_blank">
         GitHub
       </a>
       !
