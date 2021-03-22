2fd0a5efcfeb86c6e3f9e8d4ca535a7a19a08164
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-03-22 08:54:01 -0700

    pc - use lombok to simplify SearchResult class

--- src/main/java/edu/ucsb/mapache/google/SearchResult.java
+++ src/main/java/edu/ucsb/mapache/google/SearchResult.java
@@ -1,17 +1,18 @@
 package edu.ucsb.mapache.google;
 
-import java.util.Objects;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.util.JSON;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lombok.Data;
+
+@Data
 public class SearchResult {
 
     private static final Logger logger = LoggerFactory.getLogger(SearchResult.class);
@@ -30,56 +31,6 @@ public class SearchResult {
         this.url = url;
         this.queries = queries;
         this.items = items;
-    }
-
-    public String getKind() {
-        return this.kind;
-    }
-
-    public void setKind(String kind) {
-        this.kind = kind;
-    }
-
-    public Url getUrl() {
-        return this.url;
-    }
-
-    public void setUrl(Url url) {
-        this.url = url;
-    }
-
-    public Queries getQueries() {
-        return this.queries;
-    }
-
-    public void setQueries(Queries queries) {
-        this.queries = queries;
-    }
-
-    public List<Item> getItems() {
-        return this.items;
-    }
-
-    public void setItems(List<Item> items) {
-        this.items = items;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof SearchResult)) {
-            return false;
-        }
-        SearchResult searchResult = (SearchResult) o;
-        return Objects.equals(kind, searchResult.kind) && Objects.equals(url, searchResult.url)
-                && Objects.equals(queries, searchResult.queries) && Objects.equals(items, searchResult.items);
-    }
-
-    @Override
-    public String toString() {
-        return "{" + " kind='" + getKind() + "'" + ", url='" + getUrl() + "'" + ", queries='" + getQueries() + "'"
-                + ", items='" + getItems() + "'" + "}";
     }
 
     /**
