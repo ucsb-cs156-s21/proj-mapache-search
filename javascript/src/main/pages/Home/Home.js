50448655c1f0c32182dc77576637edcd59554bc0
michaelwyl <yilin_wang@ucsb.edu>
2021-05-12 13:06:32 -0700

    removed jumbotron

--- javascript/src/main/pages/Home/Home.js
+++ javascript/src/main/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Jumbotron } from "react-bootstrap";
 import {useAuth0} from "@auth0/auth0-react";
 import { CardDeck, Card } from "react-bootstrap";
 
@@ -7,13 +6,6 @@ const Home = () => {
     const { isAuthenticated: _isAuthenticated } = useAuth0();
 
     return (
-            // <Jumbotron>
-            //     <div className="text-left">
-            //         <h5>Welcome to the Mapache Search!</h5>
-            //         <p>This is where your home page content goes
-            //         </p>
-            //     </div>
-            // </Jumbotron>
             <div>
       <h1>Mapache Search</h1>
       <CardDeck>
