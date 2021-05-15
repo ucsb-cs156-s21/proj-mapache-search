e1f8e0119612a3036011209d58507bdd1934faeb
YUYUAN WANG <yuyuanwang@ucsb.edu>
2021-05-14 23:16:23 -0700

    initial commit

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -3,6 +3,7 @@ import React from "react";
 const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
+      <p>
       This app is a class project of {" "}
       <a href="https://ucsb-cs156.github.io" target="_blank" rel="noreferrer noopener">
         CMPSC 156 
@@ -15,7 +16,18 @@ const AppFooter = () => {
       <a href="https://github.com/ucsb-cs156-w21/proj-mapache-search" target="_blank" rel="noreferrer noopener">
         GitHub
       </a>
-      !
+      .
+      </p>
+      <p>
+        The cartoon Mapache images in the brand logo and magnifying glass favicon for this site were
+        developed by Chelsea Lyon-Hayden, Art Director for UCSB Associate Students, and are
+        used here by permission of the Executive Director of UCSB Associated Students.
+        These images are Copyright © 2021 UCSB Associated Students, and may not be reused
+        without express written permission of the Executive Director of UCSB Associated Students.  For more info, visit: 
+        
+        <a href="https://www.as.ucsb.edu/sticker-packs/>">www.as.ucsb.edu/sticker-packs/</a>
+        </p>
+
     </footer>
   );
 };
