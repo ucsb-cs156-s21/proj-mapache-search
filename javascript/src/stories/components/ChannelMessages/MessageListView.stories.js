77505251043194dbb48d48e981d93b07c7411987
Max Dei Rossi <m_deirossi@ucsb.edu>
2021-05-17 22:16:29 -0700

    md - add storybook story for MessageListView component

---
+++ javascript/src/stories/components/ChannelMessages/MessageListView.stories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import MessageListView from "main/components/ChannelMessages/MessageListView";
+
+
+export default {
+  title: 'components/ChannelMessages/MessageListView',
+  component: MessageListView
+};
+
+const Template = (args) => <MessageListView {...args}/>;
+
+export const List = Template.bind({});
+
+List.args = {
+  messages: [{
+    "type": "message",
+    "subtype": "channel_join",
+    "ts": "1594143066.000200",
+    "user": "U017218J9B3",
+    "text": "<@U017218J9B3> has joined the channel",
+    "channel": "section-6pm",
+    "user_profile": {
+      "real_name": "Test Person"
+    }
+  },
+  {
+    "type": "message",
+    "subtype": "channel_join",
+    "ts": "1594143066.000200",
+    "user": "U017218J9B3",
+    "text": "Office hours at <https://ucsb.zoom.us/j/89220034995?pwd=VTlHNXJpTVgrSEs5QUtlMDdqMC9wQT09>",
+    "channel": "section-6pm",
+    "user_profile": {
+      "real_name": "Test Person"
+    }
+  }],
+}
