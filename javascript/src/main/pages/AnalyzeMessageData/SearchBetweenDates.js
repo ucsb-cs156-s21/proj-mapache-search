b3a43b87729065b3c0bef4b2d8e91aff71bf3b63
Khalid Mihlar <khalid.mihlar@gmail.com>
2021-05-19 00:35:33 -0700

    km & at - made change to test on heroku due to local currently not being the best

--- javascript/src/main/pages/AnalyzeMessageData/SearchBetweenDates.js
+++ javascript/src/main/pages/AnalyzeMessageData/SearchBetweenDates.js
@@ -32,7 +32,7 @@ const SearchBetweenDates = () => {
             <Form onSubmit={handleSearchDateOnSubmit}>
                 <Form.Group controlId="searchDate" onChange={handleSearchDateOnChange}>
                     <Form.Label>Search Between Two Dates</Form.Label>
-                        <Form.input type="date"></Form.input>
+                        <Form.Control type="text" placeholder="Enter Search User" />
                         <Button onClick={handleSearchDateOnSubmit}>Search</Button>
                 </Form.Group>
             </Form>
