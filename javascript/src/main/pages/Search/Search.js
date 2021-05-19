6b1ed01a938e327a5b9c6bd0728ff8630fe15af9
an cao <an@cs.ucsb.edu>
2021-05-19 10:11:34 +0800

    Revert changes to package-lock.json

--- javascript/src/main/pages/Search/Search.js
+++ javascript/src/main/pages/Search/Search.js
@@ -87,7 +87,7 @@ const Search = () => {
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
-                    <Col sm={{ span: 10, offset: 6 }}>
+                    <Col sm={{ span: 10, offset: 7 }}>
                         <Button type="submit">Submit</Button>
                     </Col>
                 </Form.Group>
