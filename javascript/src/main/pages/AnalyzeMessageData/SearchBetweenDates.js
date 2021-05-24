10b618cf69b6e2193c873007383806602e098444
alvintu@umail.ucsb.edu <alvintu@Alvins-Laptop.localdomain>
2021-05-24 13:37:23 -0700

    at and km - corrected the syntax for the searchBetweenDates.js form

--- javascript/src/main/pages/AnalyzeMessageData/SearchBetweenDates.js
+++ javascript/src/main/pages/AnalyzeMessageData/SearchBetweenDates.js
@@ -7,16 +7,18 @@ import DateChannelMessageList from "../../components/DateMessages/DateChannelMes
 
 const SearchBetweenDates = () => {
     const { getAccessTokenSilently: getToken } = useAuth0();
-    const [ searchDate, setSearchDate ] = useState('');
+    const [ searchDate, setSearchDate ] = useState(''); 
+    const [ searchDate2, setSearchDate2 ] = useState('');
     const [ searchResults, setSearchResults ] = useState([]);
 
     const handleSearchDateOnChange = (event) => {
         setSearchDate(event.target.value);
+        setSearchDate2(event.target.value);
     };
     
     const handleSearchDateOnSubmit = (event) => {
         event.preventDefault();
-        const url = `/api/members/messages/datesearch?searchDate=${searchDate}`;
+        const url = `/api/members/messages/datesearch?searchDate=${searchDate}&searchDate2=${searchDate2}`;
         const options = {
             method: 'GET',
         }
@@ -29,13 +31,16 @@ const SearchBetweenDates = () => {
     return (
         <>
             <h1> Search Results </h1>
-            <Form onSubmit={handleSearchDateOnSubmit}>
-                <Form.Group controlId="searchDate" onChange={handleSearchDateOnChange}>
-                    <Form.Label>Search Between Two Dates</Form.Label>
+            <Form onSubmit={handleSearchDateOnSubmit} onChange={handleSearchDateOnChange}>
+                <Form.Group controlId="searchDate">
+                    <Form.Label>Start date</Form.Label>
                         <Form.Control type="date" placeholder="mm/dd/yyyy" />
-                        <Form.Control type="date" placeholder="mm/dd/yyyy" />
-                        <Button onClick={handleSearchDateOnSubmit}>Search</Button>
                 </Form.Group>
+                <Form.Group controlId="searchDate2">
+                    <Form.Label>End date</Form.Label>
+                        <Form.Control type="date" placeholder="mm/dd/yyyy" />
+                </Form.Group>
+                <Button onClick={handleSearchDateOnSubmit}>Search</Button>
             </Form>
             <DateChannelMessageList messages = {searchResults} />
             <DateMessageList messages = {searchResults} />
