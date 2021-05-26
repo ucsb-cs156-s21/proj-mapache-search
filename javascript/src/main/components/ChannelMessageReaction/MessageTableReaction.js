dc39351628cdda11fe18a4ba2207c9db09cd4ab3
Jason Dunne <jasonrdunne@csil.cs.ucsb.edu>
2021-05-25 19:38:54 -0700

    JD - On analyze reactions page: links are clickable, users show up as their profile name

--- javascript/src/main/components/ChannelMessageReaction/MessageTableReaction.js
+++ javascript/src/main/components/ChannelMessageReaction/MessageTableReaction.js
@@ -1,8 +1,67 @@
 import React from "react";
-import BootstrapTable  from 'react-bootstrap-table-next';
+import BootstrapTable from 'react-bootstrap-table-next';
+import {useAuth0} from "@auth0/auth0-react";
+import useSWR from "swr";
+import {fetchWithToken} from "main/utils/fetch";
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+const { SearchBar } = Search;
+
+const GetMessageContents = (text, slackUsers) => {
+    return text.replace(/<@([A-Z0-9]{11})>/g, (_,userId) => {
+        if(slackUsers != null) {
+            for(let i = 0; i < slackUsers.length; i++) {
+                if(slackUsers[i].id === userId) {
+                    return "@" + slackUsers[i].real_name;
+                }
+            }
+        }
+        return "@" + userId;
+    })
+}
+
+const formatBracketedText = (text) => {
+    var bracketRegEx = /<(.*?)>/g;
+    var found = text.match(bracketRegEx)
+    if (found){
+        for (let i=0; i<found.length; i++){
+            var current = found[i].replace('<', '');
+            current = current.replace('>', '');
+            if (found[i].includes("|") && (found[i].includes("mailto") || found[i].includes("http") || found[i].includes("tel"))){      // embedded links
+                var links = current.split('|');
+                text = text.replace(found[i], '<a href = ' + links[0] + ' target = "_blank">' + links[1] +'</a>')
+            }else if (found[i].includes("http") || found[i].includes("mailto") || found[i].includes("tel")){                            // unembedded links
+                text = text.replace(found[i], '<a href = ' + current + ' target = "_blank">' + current + '</a>')
+            }else if (found[i].includes("|")){                                                                                          // channel links
+                links = current.split('|');
+                text = text.replace(found[i], '<a href = /member/listViewChannels/' + links[1] + '>#' + links[1] + '</a>')
+            }else if (found[i].includes("!")){                                                                                          // channel tags (ex: @channel)
+                text = text.replace(found[i], '<strong> @' + current + '</strong>')
+                text = text.replace("!", "")
+            }else{
+                text = text.replace(found[i], current)
+            }
+        }
+    }
+    return text;
+}
+
+const createMarkup = (text, slackUsers) => {
+    text = GetMessageContents(text, slackUsers)
+    text = formatBracketedText(text)
+    return {
+        __html: text
+    }
+}
+
+function nameFormatter(value, row) {
+    value = row.user;
+    return row.user_profile? row.user_profile.real_name:value;
+}
 
 export default ({ messages=[],reaction }) => {
-
+    const { getAccessTokenSilently: getToken } = useAuth0();
+    const {data: slackUsers} = useSWR([`/api/slackUsers`, getToken], fetchWithToken);
+    
     function subjectFormatter(_cell, row, _rowIndex) {
         const correctReaction = row.message_reactions.find((message_reaction) => {
             return message_reaction.name === reaction;
@@ -11,9 +70,11 @@ export default ({ messages=[],reaction }) => {
     }
     const columns = [{
         dataField: 'user',
+        formatter: nameFormatter,
         text: 'Message Author'
     },{
         dataField: 'text',
+        formatter: (_cell, row) => <p dangerouslySetInnerHTML = {createMarkup(row.text, slackUsers)}></p>,
         text: 'Contents'
     },{
         dataField: 'reactions',
