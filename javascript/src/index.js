aa36e1a65448562f3fb6651f1c39e9052f243bf5
an cao <an@cs.ucsb.edu>
2021-05-27 08:53:57 +0800

    fixing useeffect and index.js

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -7,16 +7,17 @@ import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { ToastProvider } from "react-toast-notifications";
+
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <Auth0ProviderWithHistory>
-      <ToastProvider autoDismiss={true} autoDismissTimeout={10000}>
-        <App />
-      </ToastProvider>
-    </Auth0ProviderWithHistory>
-  </BrowserRouter>
-</React.StrictMode>,
+    <BrowserRouter>
+      <Auth0ProviderWithHistory>
+        <ToastProvider autoDismiss={true} autoDismissTimeout={10000}>
+          <App />
+        </ToastProvider>
+      </Auth0ProviderWithHistory>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
--- javascript/src/main/pages/Search/Search.js
+++ javascript/src/main/pages/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { fetchWithToken } from "main/utils/fetch";
