50df3d7d2d94b61e96caff84c205c8fe333a5f43
Nico Wong <nicowong8@gmail.com>
2021-05-26 19:10:22 -0700

    nw - Adds query to SlackUserRepository to find Slack admins with a given a email

--- src/main/java/edu/ucsb/mapache/repositories/SlackUserRepository.java
+++ src/main/java/edu/ucsb/mapache/repositories/SlackUserRepository.java
@@ -12,4 +12,7 @@ import java.util.Optional;
 public interface SlackUserRepository extends MongoRepository<SlackUser, ObjectId> {
     @Query("{ 'profile.email': ?0}")
     List<SlackUser> findByEmail(String email);
+
+    @Query("{ 'is_admin': true, 'profile.email': ?0")
+    List<SlackUser> findAdminByEmail(String email);
 }
