df631b6ebff9f6fad2ea2bffbe56331c344b4ed7
Reiley Batelaan <reileybatelaan@gmail.com>
2021-05-27 09:54:07 -0700

    rb - added some test cases

--- javascript/src/test/pages/AnalyzeMessageData/HistogramOfMessagesByUser.test.js
+++ javascript/src/test/pages/AnalyzeMessageData/HistogramOfMessagesByUser.test.js
@@ -2,8 +2,44 @@ import React from "react";
 import { render } from "@testing-library/react";
 import HistogramOfMessagesByUser from "main/pages/AnalyzeMessageData/HistogramOfMessagesByUser";
 
+jest.mock("@auth0/auth0-react");
+
+jest.mock("main/utils/fetch", () => ({
+  fetchWithToken: jest.fn()
+}));
+
 describe("HistogramOfMessagesByUser tests", () => {
-    test("renders without crashing", () => {
-      render(<HistogramOfMessagesByUser/>);
+  beforeEach(() => {
+    useAuth0.mockReturnValue({
+      getAccessTokenSilently: "fakeToken"
     });
+  });  
+  
+  test("renders without crashing", () => {
+    render(<HistogramOfMessagesByUser/>);
+  });
+
+  test("renders without crashing on search for user", async () => {
+    const { getByLabelText } = render(<HistogramOfMessagesByUser />);
+    const selectSearchUser = getByLabelText("Enter a user");
+    userEvent.type(selectSearchUser, "Test User");
+    expect(selectSearchUser.value).toBe("Test User");
+  })
+
+  test("Fetch with token is called correctly when user clicks button", async () => {
+    const expectedURL = `/api/members/messages/usersearch?searchUser=Test User`;
+    const options = {
+      method: 'GET'
+    }
+    fetchWithToken.mockResolvedValue([]);
+    const { getByText, getByLabelText } = render(<HistogramOfMessagesByUser />);
+    const selectSearchUser = getByLabelText("Enter a user");
+    userEvent.type(selectSearchUser, "Test User");
+    const goButton = getByText("Go");
+    userEvent.click(goButton);
+    await waitFor(() => {
+      expect(fetchWithToken).toHaveBeenCalledTimes(1);
+      expect(fetchWithToken).toHaveBeenCalledWith(expectedURL, "fakeToken", options);
+    })
+  })
 });
