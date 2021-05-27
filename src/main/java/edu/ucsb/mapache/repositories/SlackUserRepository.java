6124f59ec89c7b228f93c0c501e90d480bd66a9a
Karan <karankhangura@ucsb.edu>
2021-05-27 12:41:18 -0700

    7pm-4 kk - fix merge conflict

--- src/main/java/edu/ucsb/mapache/repositories/SlackUserRepository.java
+++ src/main/java/edu/ucsb/mapache/repositories/SlackUserRepository.java
@@ -13,5 +13,8 @@ public interface SlackUserRepository extends MongoRepository<SlackUser, ObjectId
     @Query("{ 'profile.email': ?0}")
     List<SlackUser> findByEmail(String email);
 
+    @Query("{ 'is_admin': true}")
+    List<SlackUser> findAdmins();
+
     public List<SlackUser> findByName(String name);
 }
