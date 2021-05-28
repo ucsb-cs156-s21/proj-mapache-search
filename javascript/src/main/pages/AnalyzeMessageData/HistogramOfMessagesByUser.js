746d3d59a1352c52249f92b3d42ce4cee4b25904
Reiley Batelaan <reileybatelaan@gmail.com>
2021-05-27 17:45:35 -0700

    rb - work in progress code

--- javascript/src/main/pages/AnalyzeMessageData/HistogramOfMessagesByUser.js
+++ javascript/src/main/pages/AnalyzeMessageData/HistogramOfMessagesByUser.js
@@ -3,6 +3,7 @@ import { fetchWithToken } from "../../utils/fetch";
 import { useAuth0 } from "@auth0/auth0-react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import { Row, Col, Button, Form } from "react-bootstrap";
+import {Bar} from 'react-chartjs-2';
 
 const columns = [{
     dataField: 'week',
@@ -21,6 +22,7 @@ const HistogramOfMessagesByUser = () => {
     const [selectedUser, setSelectedUser] = useState('');
     const [inputName, setInputName] = useState('');
     const [perWeekHistogramData, setPerWeekHistogramData] = useState([]);
+    const [histogramChartData, setHistogramChartData] = useState();
 
     const handleSelectUserOnSubmit = async () => { 
         setSelectedUser(inputName);
@@ -48,13 +50,45 @@ const HistogramOfMessagesByUser = () => {
             week: week,
             count: histogramData[week]
         }));
-
         setPerWeekHistogramData(tableData);
+
+
+        const label = [];
+        const datas = [];
+
+        tableData.forEach(i => {
+            label.push(i.week)
+        })
+
+        tableData.forEach(i => {
+            datas.push(i.count)
+        })
+        
+        let chartData = {
+            labels: label,
+            datasets: [{
+                label: "Messages per Week",
+                data: datas,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                borderWidth: 1
+            }],
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            },
+            order: 0,
+            padding: 5,
+        };
+        setHistogramChartData(chartData);
     };
 
     return (
         <div>
-            <h3>Histogram of Messages By User</h3>
+            <h1>Histogram of Messages By User</h1>
             <Row>
                 <Col>
                     <Form.Group controlId="form-select" className="pt-3" style={{ textAlign: "left" }}>
@@ -70,7 +104,22 @@ const HistogramOfMessagesByUser = () => {
             </Row>
             { selectedUser !== '' &&
                 <div>
-                    <h3>Activity for {selectedUser}</h3>
+                    <h3>Histogram of Activity for {selectedUser}</h3>
+                    <Bar
+                        data={histogramChartData}
+                        options={{
+                        title:{
+                            display:true,
+                            text:'Histogram Chart',
+                            fontSize:20
+                        },
+                        legend:{
+                            display:true,
+                            position:'right'
+                        }
+                        }}
+                    />
+                    <h3>Table of Activity for {selectedUser}</h3>
                     <BootstrapTable keyField='week' data={perWeekHistogramData} columns={columns}></BootstrapTable>
                 </div>
             }
@@ -78,4 +127,4 @@ const HistogramOfMessagesByUser = () => {
     );
 };
 
-export default HistogramOfMessagesByUser;
+export default HistogramOfMessagesByUser;        
