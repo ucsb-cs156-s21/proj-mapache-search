8cd246bc2bdab160228211013033fa699ca78bb6
Emily O'Mahony <emilyomahony@gmail.com>
2021-05-28 15:22:01 -0700

    7pm-1 eo - further removed unnecessary code

--- javascript/src/main/components/ChannelMessages/ChannelLinks.js
+++ javascript/src/main/components/ChannelMessages/ChannelLinks.js
@@ -1,8 +1,5 @@
 import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
-import {useAuth0} from "@auth0/auth0-react";
-import useSWR from "swr";
-import {fetchWithToken} from "main/utils/fetch";
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 import TimeFormatter from "./time"
 const { SearchBar } = Search;
@@ -13,13 +10,11 @@ const filterLinks = (messages) => {
     let link_messages = messages.filter(message => (message.text.includes("<http") && message.text.includes(">")));
     let bracketRegEx = /<http(.*?)>/g;
     let links = [];
-    let found;
-    let new_message;
     // for each message, for each link in it, strip other text & add to links
     link_messages.forEach(function(element) {
-        found = element.text.match(bracketRegEx);
+        let found = element.text.match(bracketRegEx);
         found.forEach(function(part) {
-            new_message = { ...element };
+            let new_message = { ...element };
             new_message.text = part;
             links.push(new_message);
         })
@@ -34,20 +29,18 @@ const formatBracketedText = (text) => {
         for (let i=0; i<found.length; i++){
             let current = found[i].replace('<', '');
             current = current.replace('>', '');
-            if (found[i].includes("|") && (found[i].includes("http"))){      // embedded links
+            if (found[i].includes("|") && (found[i].includes("http"))){                                          // embedded links
                 let links = current.split('|');
                 text = text.replace(found[i], '<a href = ' + links[0] + ' target = "_blank">' + links[1] +'</a>')
-            } else if (found[i].includes("http")){                            // unembedded links
+            } else if (found[i].includes("http")){                                                               // unembedded links
                 text = text.replace(found[i], '<a href = ' + current + ' target = "_blank">' + current + '</a>')
-            } else {
-                text = text.replace(found[i], current)
             }
         }
     }
     return text;
 }
 
-const createMarkup = (text, slackUsers) => {
+const createMarkup = (text) => {
     text = formatBracketedText(text)
     return {
         __html: text
@@ -60,10 +53,7 @@ function nameFormatter(value, row) {
 }
 
 export default ({ messages }) => {
-    const { getAccessTokenSilently: getToken } = useAuth0();
-    const {data: slackUsers} = useSWR([`/api/slackUsers`, getToken], fetchWithToken);
     const links = filterLinks(messages);
-    
     const columns = [{
         isDummyField: true,
         formatter: nameFormatter,
@@ -71,7 +61,7 @@ export default ({ messages }) => {
         text: 'Username'
     },{
         isDummyField: true,
-        formatter: (_cell, row) => <p dangerouslySetInnerHTML = {createMarkup(row.text, slackUsers)}></p>,
+        formatter: (_cell, row) => <p dangerouslySetInnerHTML = {createMarkup(row.text)}></p>,
         dataField: 'text',
         text: 'URL',
         sort: true
