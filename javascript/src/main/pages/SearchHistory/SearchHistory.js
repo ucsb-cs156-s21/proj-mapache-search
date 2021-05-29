3a66d8a030a9158d1d429cb3336a347c62b8b157
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-05-29 12:48:59 +0800

    Update SearchHistory.js

--- javascript/src/main/pages/SearchHistory/SearchHistory.js
+++ javascript/src/main/pages/SearchHistory/SearchHistory.js
@@ -1,1 +1,36 @@
+import React from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+import useSWR from "swr";
+import {useAuth0} from "@auth0/auth0-react";
+import {fetchWithToken} from "main/utils/fetch";
 
+const SearchHistory = () => {
+    const columns = [{
+        dataField: 'UserID',
+        text: 'UserID',
+        sort: true
+    } , {
+        dataField: "searchTerm",
+        text: 'Searchquery',
+        sort: true
+    },{
+       dataField: "timestamp" ,
+       text: 'Timestamp' ,
+       sort: true
+    }];
+
+    const { getAccessTokenSilently: getToken } = useAuth0();
+    
+    const { data: slackUsers } = useSWR(["/api/slackUsers", getToken], fetchWithToken);
+    const { data: messages } = useSWR(["/api/members/messages/allmessages", getToken], fetchWithToken);
+
+    const userMessageCount = messages && slackUsers ? aggregateUserMessageCount(messages, slackUsers) : []
+
+    return (
+        <div>
+            <h1>Show Search History</h1>
+            <BootstrapTable keyField='id' data={userMessageCount} columns={columns} />
+        </div>
+    );
+};
+export default SearchHistory;
