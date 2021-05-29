d2265dc3c2f595372a52b08c53e4e58310853732
Vanessa-Salgado <vsalgadozavaleta@ucsb.edu>
2021-05-29 16:07:35 -0700

    vs - implemented MembersListSerice

--- src/main/java/edu/ucsb/mapache/services/MembersListService.java
+++ src/main/java/edu/ucsb/mapache/services/MembersListService.java
@@ -6,16 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.ucsb.mapache.entities.Student;
-import edu.ucsb.mapache.repositories.StudentRepository;
+import edu.ucsb.mapache.repositories.TeamRepository;
 
 @Service
 public class MembersListService {
     
     @Autowired
-    private StudentRepository studentRepository;
+    private TeamRepository teamRepository;
 
     public String getListOfMembers() {
-        
+        List<Student> studentsAll = teamRepository.findByTeamName();
+        String teamMembersList = "";
+        for (Student s : studentsAll) {
+            teamMembersList += s.getTeamName() + "\n";
+    }
+        return teamMembersList;
+  }
 
     }
 }
