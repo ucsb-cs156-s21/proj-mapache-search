061f0334cba0e1ab5e4ce1f6f117707e09de54f2
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-05-29 14:23:42 +0800

    Create UserSearchRepository.java

---
+++ src/main/java/edu/ucsb/mapache/repositories/UserSearchRepository.java
@@ -0,0 +1,15 @@
+package edu.ucsb.mapache.repositories;
+
+import edu.ucsb.mapache.entities.UserSearch;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+public interface UserSearchRepository extends CrudRepository<UserSearch, Long> {
+  public List<UserSearch> findAll();
+}
