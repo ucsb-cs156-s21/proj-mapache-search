3e1993228efe45b93f3f027b952a5e4dd03ff9ce
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-05-29 22:13:57 +0800

    Update UserSearchRepository.java

--- src/main/java/edu/ucsb/mapache/repositories/UserSearchRepository.java
+++ src/main/java/edu/ucsb/mapache/repositories/UserSearchRepository.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 
 public interface UserSearchRepository extends CrudRepository<UserSearch, Long> {
   public List<UserSearch> findAll();
-  public List<UserSearch> findByUserId(String userId);
+  public List<UserSearch> findByUserID(String userID);
 }
