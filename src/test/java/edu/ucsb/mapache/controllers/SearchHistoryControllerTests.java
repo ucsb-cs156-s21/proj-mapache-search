53a9fa64d0456a3413b850983905b9ffbfa46fbe
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-05-31 03:12:10 +0800

    Update SearchHistoryControllerTests.java

--- src/test/java/edu/ucsb/mapache/controllers/SearchHistoryControllerTests.java
+++ src/test/java/edu/ucsb/mapache/controllers/SearchHistoryControllerTests.java
@@ -56,6 +56,9 @@ public class SearchHistoryControllerTests {
   @Autowired
   private SearchHistoryController searchhistorycontoller;
   
+  @Autowired
+  private ObjectMapper objectMapper;
+  
   @MockBean
   AppUserRepository appUserRepository;
   @MockBean
