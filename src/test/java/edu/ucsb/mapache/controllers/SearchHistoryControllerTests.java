286e2b8eda7da23324225a7b645e3b48dedbb946
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-05-31 00:47:49 +0800

    Update SearchHistoryControllerTests.java

--- src/test/java/edu/ucsb/mapache/controllers/SearchHistoryControllerTests.java
+++ src/test/java/edu/ucsb/mapache/controllers/SearchHistoryControllerTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
