90f7a2a15f1d478d501ccfb7a7021393144cd8ad
Yilin Wang <michael@Macintosh.local>
2021-05-30 22:59:04 +0800

    added search history test

---
+++ javascript/src/test/pages/SearchHistory/SearchHistory.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { act, render } from '@testing-library/react';
+import SearchHistory from 'main/pages/SearchHistory/SearchHistory';
+
+describe('Slack Users tests', () => {
+    act(()=>{
+        test('renders without errors', () => {
+            render(<SearchHistory />);
+        });
+    });   
+});
+
