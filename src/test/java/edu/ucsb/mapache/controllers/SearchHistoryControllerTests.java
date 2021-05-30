8fbabdf5ca7a372fd9c518d1f3014d582bc48f78
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-05-31 01:10:27 +0800

    Update SearchHistoryControllerTests.java

--- src/test/java/edu/ucsb/mapache/controllers/SearchHistoryControllerTests.java
+++ src/test/java/edu/ucsb/mapache/controllers/SearchHistoryControllerTests.java
@@ -12,6 +12,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.mockito.InjectMocks;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
