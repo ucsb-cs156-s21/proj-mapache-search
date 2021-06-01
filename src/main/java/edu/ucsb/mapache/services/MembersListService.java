632983cf5796bb75f683a372ed5b7ee4ce41f4ac
karankhangura <77126503+karankhangura@users.noreply.github.com>
2021-06-01 16:00:03 -0700

    MembersListService actually gets Student names now

--- src/main/java/edu/ucsb/mapache/services/MembersListService.java
+++ src/main/java/edu/ucsb/mapache/services/MembersListService.java
@@ -1,11 +1,12 @@
 package edu.ucsb.mapache.services;
 
 import java.util.List;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.ucsb.mapache.documents.SlackUser;
+import edu.ucsb.mapache.repositories.SlackUserRepository;
 import edu.ucsb.mapache.entities.Student;
 import edu.ucsb.mapache.repositories.StudentRepository;
 
@@ -14,13 +15,28 @@ public class MembersListService {
     
     @Autowired
     private StudentRepository studentRepository;
+    
+    @Autowired
+    private SlackUserRepository slackUserRepository;
+    
     public String getListOfMembers(String teamName) {
         List<Student> studentsAll = studentRepository.findByTeamName(teamName);
-        Collections.sort(studentsAll);
-        String teamMembersList = "";
-        for (Student s : studentsAll) {
-            teamMembersList += s + "\n";
-    }
-        return teamMembersList;
+        List<String> emailList = new String<List>();
+        for (Student s : students) {
+            studentsAll.add(s.getEmail());
+        }
+        List<String> outputList = new String<List>();
+        for(String x: emailList){
+            List<SlackUser> user = slackUserRepository.findByEmail(x);
+            outputList.add(user.get(0).getProfile().getReal_name());
+        }
+        
+        java.util.Collections.sort(outputList);
+        String output = ""
+        for (String s : outputList) {
+             output += s + "\n";
+        }
+        
+        return output;
   }
 }
