a9e855fafa87c24909df5f5586baba3cf7dc6637
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-06-03 15:33:43 +0800

    Update SearchHistory.test.js

--- javascript/src/test/pages/SearhHistory/SearchHistory.test.js
+++ javascript/src/test/pages/SearhHistory/SearchHistory.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import SearchHistory from 'main/pages/SearchHistory/SearchHistory';
 import useSWR from "swr";
+import { useParams} from "react-router-dom";
 
 jest.mock("swr");
+jest.mock("react-router-dom", () => {
+    return {
+        'useParams': jest.fn(),
+    };
+});
 
 describe('Search History tests', () => {
    
