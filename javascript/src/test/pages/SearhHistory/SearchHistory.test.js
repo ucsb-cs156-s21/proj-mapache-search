4d76053d550b3febd7481f41e515e262bb8a36b6
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-06-03 15:57:25 +0800

    Update SearchHistory.test.js

--- javascript/src/test/pages/SearhHistory/SearchHistory.test.js
+++ javascript/src/test/pages/SearhHistory/SearchHistory.test.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import SearchHistory from 'main/pages/SearchHistory/SearchHistory';
-import useSWR from "swr";
+//import useSWR from "swr";
 
-jest.mock("swr");
+//jest.mock("swr");
 
 
-describe('Search History tests', () => {
-   
-        test('test as member', () => {
-            useSWR.mockReturnValue({
-      data: {
-        role: "member"
-      }
-    });
-            const { getByText } = render(<SearchHistory />);
-    expect(getByText("Member")).toBeInTheDocument();
+describe('Slack Users tests', () => {
+  
+        test('renders without errors', () => {
+            render(<SearchHistory />);
         });
-            
-           test("test as admin", () => {
-        useSWR.mockReturnValue({
-      data: {
-        role: "admin"
-      }
-    });
-        const { getByText } = render(<SearchHistory />);
-    expect(getByText("Admin")).toBeInTheDocument();
-    });
-    
-     
+   
 });
