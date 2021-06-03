a78bfbff1ed6c6439d8f4464facd026a7a36d13a
bowenduanmu <bowenduanmu@umail.ucsb.edu>
2021-06-03 14:08:03 +0800

    update SearchHistory page and corresponding controller and test

--- javascript/src/main/pages/SearchHistory/SearchHistory.js
+++ javascript/src/main/pages/SearchHistory/SearchHistory.js
@@ -1,13 +1,14 @@
 import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
+import { Link } from 'react-router-dom';
 import useSWR from "swr";
 import {useAuth0} from "@auth0/auth0-react";
 import {fetchWithToken} from "main/utils/fetch";
 
 const SearchHistory = () => {
     const columns = [{
-        dataField: 'email',
-        text: 'Email',
+        dataField: 'userID',
+        text: 'UserID',
         sort: true
     } , {
         dataField: 'searchTerm',
@@ -18,10 +19,31 @@ const SearchHistory = () => {
         text: 'Timestamp' ,
         sort: true
     }];
-
+    
+ 
+        
     const { getAccessTokenSilently: getToken } = useAuth0();
     
-    const { data: usersearch } = useSWR(["/api/members/searchhistory/allusersearches", getToken], fetchWithToken);
+    const { data: usersearch } = useSWR(["/api/members/searchhistory/specificuser", getToken], fetchWithToken);
+    
+     const { data: roleInfo } = useSWR(
+        ["/api/myRole", getToken],
+        fetchWithToken
+      );
+      const isAdmin = roleInfo && roleInfo.role.toLowerCase() === "admin";
+      
+   
+    if(isAdmin){
+         return (
+            <div>
+                <h1>Show Search History</h1>
+                <Link to="/member/searchhistory/alluser" className="btn btn-primary">ViewAllUsers</Link>
+
+                
+        <BootstrapTable keyField='id' data={usersearch || []} columns={columns} />
+            </div>
+        );
+    }
     
     return (
         <div>
