5a4e6d0808da34de10f4eb06294b3bd9b376a007
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-06-03 15:11:48 +0800

    Update SearchHistory.test.js

--- javascript/src/test/pages/SearhHistory/SearchHistory.test.js
+++ javascript/src/test/pages/SearhHistory/SearchHistory.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { act, render } from '@testing-library/react';
 import SearchHistory from 'main/pages/SearchHistory/SearchHistory';
+import useSWR from "swr";
+jest.mock("@auth0/auth0-react");
+jest.mock("swr");
 
-describe('Slack Users tests', () => {
+describe('Search History tests', () => {
     act(()=>{
-        test('renders without errors', () => {
+        test('test as member', () => {
             render(<SearchHistory />);
         });
-    });   
+            
+           test("test as admin", () => {
+        useSWR.mockReturnValue({
+      data: {
+        role: "admin"
+      }
+    });
+        render(<SearchResults />);
+    });
+    
+   });   
 });
