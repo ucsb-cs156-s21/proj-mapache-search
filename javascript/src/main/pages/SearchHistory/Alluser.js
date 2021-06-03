232cc872fb4f463bb159b32e4601094ea0317877
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-06-03 12:36:42 +0800

    Create Alluser.js

---
+++ javascript/src/main/pages/SearchHistory/Alluser.js
@@ -0,0 +1,33 @@
+import React from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+import useSWR from "swr";
+import {useAuth0} from "@auth0/auth0-react";
+import {fetchWithToken} from "main/utils/fetch";
+
+const Alluser = () => {
+    const columns = [{
+        dataField: 'userID',
+        text: 'UserID',
+        sort: true
+    } , {
+        dataField: 'searchTerm',
+        text: 'Searchquery',
+        sort: true
+    },{
+        dataField: 'timestamp' ,
+        text: 'Timestamp' ,
+        sort: true
+    }];
+
+    const { getAccessTokenSilently: getToken } = useAuth0();
+    
+    const { data: usersearch } = useSWR(["/api/members/searchhistory/allusersearches", getToken], fetchWithToken);
+    
+    return (
+        <div>
+            <h1>Show Search History of All Users</h1>
+            <BootstrapTable keyField='id' data={usersearch || []} columns={columns} />
+        </div>
+    );
+};
+export default Alluser;
