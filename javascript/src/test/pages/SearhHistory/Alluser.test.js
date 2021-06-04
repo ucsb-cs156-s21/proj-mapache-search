87515e3c44b07d8ddb22480f2e213c7eaaff90b8
an cao <an@cs.ucsb.edu>
2021-06-04 08:21:15 +0800

    fixed AllUser name convention

--- javascript/src/test/pages/SearhHistory/Alluser.test.js
+++ javascript/src/test/pages/SearhHistory/Alluser.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { act, render } from '@testing-library/react';
-import Alluser from 'main/pages/SearchHistory/Alluser';
+import AllUser from 'main/pages/SearchHistory/AllUser';
 
 describe('All Users tests', () => {
     act(()=>{
         test('renders without errors', () => {
-            render(<Alluser />);
+            render(<AllUser />);
         });
     });   
 });
