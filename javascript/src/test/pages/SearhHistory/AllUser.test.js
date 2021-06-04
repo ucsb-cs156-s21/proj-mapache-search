a4b44c3472b0f3f3a9a098ee35dc7ddec4a1b06f
bowenduanmu <59592916+bowenduanmu@users.noreply.github.com>
2021-06-05 02:29:24 +0800

    change the test for the page

--- javascript/src/test/pages/SearhHistory/AllUser.test.js
+++ javascript/src/test/pages/SearhHistory/AllUser.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { act, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import AllUser from 'main/pages/SearchHistory/AllUser';
 
 describe('All Users tests', () => {
-    act(()=>{
+   
         test('renders without errors', () => {
             render(<AllUser />);
         });
-    });   
+     
 });
 
